49ff7371d034b8b46fc5f613aa70fb507800701d
Arslan Masood <arslan.masood@waseel.com>
2019-10-06 16:07:37 +0300

    Fixed import errors due to missing app directory

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { ClaimfileuploadComponent } from './app/claimfileupload/claimfileupload.component';
-import { SidebarComponent } from './app/sidebar/sidebar.component';
-import { HeaderComponent } from './app/header/header.component';
+import { ClaimfileuploadComponent } from './claimfileupload/claimfileupload.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
