500a81b756924d86f8cd7ccc4eb6087d9e811507
arslnmsd <arslnmsd@gmail.com>
2019-10-06 14:39:32 +0300

    Added bootstrap and core-js. Removed contents of app.component.html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ClaimfileuploadComponent } from './app/claimfileupload/claimfileupload.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClaimfileuploadComponent
   ],
   imports: [
     BrowserModule
