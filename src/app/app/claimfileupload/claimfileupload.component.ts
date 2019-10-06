77e2d3cff195a0400f7b5e28d519fbc3fb8137f1
Arslan Masood <arslan.masood@waseel.com>
2019-10-06 16:01:26 +0300

    Added Three Components: Sidebar, Header and FileUploader

---
+++ src/app/app/claimfileupload/claimfileupload.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-claimfileupload',
+  templateUrl: './claimfileupload.component.html',
+  styleUrls: ['./claimfileupload.component.css']
+})
+export class ClaimfileuploadComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
