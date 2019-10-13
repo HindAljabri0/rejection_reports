ad0ddd39a0cb15417ad59f5177b4704fa66703aa
Ahmed.Alyousef <ahmadalsiddeeq@gmail.com>
2019-10-13 10:44:02 +0300

    local testing changes

--- src/app/claimfileupload/claimfileupload.component.ts
+++ src/app/claimfileupload/claimfileupload.component.ts
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClaimfileuploadComponent implements OnInit {
 
+  selectedFile:File;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  detectFiles(event) {
+    console.log("testing");
+    this.selectedFile = event.target.files.item(0);
   }
 
 }
