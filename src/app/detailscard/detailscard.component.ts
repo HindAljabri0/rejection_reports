b00121763ff4faf131f4df5ad26f11de1846fee9
System Administrator <root@Waseels-iMac.local>
2019-11-07 11:58:50 +0300

    Merge

--- src/app/detailscard/detailscard.component.ts
+++ src/app/detailscard/detailscard.component.ts
@@ -1,37 +1,3 @@
-<<<<<<< HEAD
-import { Component, Input, ContentChildren, Output } from '@angular/core';
-import { EventEmitter } from 'events';
-import { MatCard } from '@angular/material/card';
-
-@Component({
-  selector: 'app-detailscard',
-  templateUrl: './detailscard.component.html',
-  styleUrls: ['./detailscard.component.css']
-})
-export class DetailscardComponent  {
-
-  constructor() { }
-
-  @Input() title: string;
-
-  @Input() topLeftActionText: string;
-  @Input() topRightActionText: string;
-
-  @Input() bottomLeftActionText: string;
-  @Input() bottomRightActionText: string;
-
-  @Input() bottomDetails: string;
-
-  @Input() accentColor:string;
-
-  @Output() topLeftAction = new EventEmitter();
-  @Output() topRightAction = new EventEmitter();
-
-  @Output() bottomLeftAction = new EventEmitter();
-  @Output() bottomRightAction = new EventEmitter();
-
-}
-=======
 import { Component, Input, Output } from '@angular/core';
 import { EventEmitter } from '@angular/core';
 
@@ -63,4 +29,3 @@ export class DetailscardComponent  {
   @Output() bottomRightAction = new EventEmitter();
 
 }
->>>>>>> 33421a4c9f5db001fc572f930973155c642dd4b9
