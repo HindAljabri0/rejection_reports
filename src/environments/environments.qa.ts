30652fced47db239c4277aeb67d25cbafb0a150e
Ahmed.Alyousef <ahmadalsiddeeq@gmail.com>
2019-12-03 14:19:35 +0300

    add qa profile. default build behavor is building qa now

---
+++ src/environments/environments.qa.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    production: true,
+    uploaderHost: "http://claim-uploader-waseele-cloud.apps.okd.waseel.com",
+    claimSearchHost: "http://claim-search-waseele-cloud.apps.okd.waseel.com",
+    claimServiceHost: "	http://claim-service-waseele-cloud.apps.okd.waseel.com",
+    NotificationServiceHost: "http://notification-service-waseele-cloud.apps.okd.waseel.com"
+  };
