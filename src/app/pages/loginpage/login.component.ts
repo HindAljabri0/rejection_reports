1e586782a474360d92c34f6a9cfae978e5c99801
Ahmed.Alyousef <ahmadalsiddeeq@gmail.com>
2019-12-08 15:05:34 +0300

    handle login errors

--- src/app/pages/loginpage/login.component.ts
+++ src/app/pages/loginpage/login.component.ts
@@ -3,6 +3,8 @@ import { FormControl } from '@angular/forms';
 import { AuthService } from 'src/app/services/authService/authService.service';
 import { HttpResponse, HttpErrorResponse } from '@angular/common/http';
 import { Router } from '@angular/router';
+import { CommenServicesService } from 'src/app/services/commen-services.service';
+import { MessageDialogData } from 'src/app/models/dialogData/messageDialogData';
 
 @Component({
     selector: 'app-login',
@@ -12,22 +14,36 @@ import { Router } from '@angular/router';
 
   export class LoginComponent {
 
-    constructor(public authService:AuthService, public router:Router){}
+    constructor(public authService:AuthService, public router:Router, public commen:CommenServicesService){}
     ngOnInit() {}
 
     username = new FormControl();
     password = new FormControl();
 
+    isLoading = false;
+
     login(){
+      if(this.isLoading) return;
+      this.isLoading = true;
+      if(this.username.value == undefined || this.username.value == "" || this.password.value == undefined || this.password.value == ""){
+        this.commen.openDialog(new MessageDialogData("", "username & password are requierd.", true));
+        this.isLoading = false;
+        return;
+      }
       this.authService.login(this.username.value, this.password.value).subscribe(event => {
         if(event instanceof HttpResponse){
           this.authService.setTokens(event.body);
           this.router.navigate(['/']);
+          this.isLoading = false;
         }
       }, errorEvent =>{
         if(errorEvent instanceof HttpErrorResponse){
-
-        }
+           if(errorEvent.status < 500 && errorEvent.status >= 400){
+            this.commen.openDialog(new MessageDialogData("", "username or password are not vaild!", true));
+          } else
+            this.commen.openDialog(new MessageDialogData("", "Could not reach server at the moment. Please try again later.", true));
+          this.isLoading = false;
+        } 
       });
     }
   }
