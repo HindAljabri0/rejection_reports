735b69b2b82e19ed9dbf899d204d650594d72603
Anas Dawood <anas_dawood@hotmail.com>
2019-12-26 15:23:29 +0300

    - Add batchId to search service

--- src/app/services/serchService/search-service.service.ts
+++ src/app/services/serchService/search-service.service.ts
@@ -7,34 +7,36 @@ import { environment } from 'src/environments/environment';
 })
 export class SearchServiceService {
 
-  constructor(private http:HttpClient) { }
+  constructor(private http: HttpClient) { }
 
-  getSummaries(providerId:string, fromDate:string, toDate:string, payerId:string, status:string){
-    const requestURL:string = '/'+providerId+'/search/claim-summary?fromDate='+fromDate
-    +'&toDate='+toDate+'&payerId='+payerId+'&status='+status;
-    const request = new HttpRequest('GET', environment.claimSearchHost+requestURL);
+  getSummaries(providerId: string, fromDate: string, toDate: string, payerId: string, status: string) {
+    const requestURL: string = '/' + providerId + '/search/claim-summary?fromDate=' + fromDate
+      + '&toDate=' + toDate + '&payerId=' + payerId + '&status=' + status;
+    const request = new HttpRequest('GET', environment.claimSearchHost + requestURL);
     return this.http.request(request);
   }
 
-  getResults(providerId:string, fromDate:string, toDate:string, payerId:string, status:string, page?:number, pageSize?:number){
-    if(page == null) page = 0;
-    if(pageSize == null) pageSize = 10;
-    const requestURL:string = '/'+providerId+'/search/claim-results?fromDate='+fromDate
-    +'&toDate='+toDate+'&payerId='+payerId+'&status='+status+'&page='+page+'&size='+pageSize;
-    const request = new HttpRequest('GET', environment.claimSearchHost+requestURL);
+  getResults(providerId: string, fromDate: string, toDate: string, payerId: string, status: string, page?: number, pageSize?: number,batchId?: number) 
+  {
+    if (page == null) page = 0;
+    if (pageSize == null) pageSize = 10;
+    const requestURL: string = '/' + providerId + '/search/claim-results?fromDate=' + fromDate
+      + '&toDate=' + toDate + '&payerId=' + payerId + '&status=' + status + '&page=' + page + '&size=' + pageSize
+      + '&batchId=' + batchId;
+    const request = new HttpRequest('GET', environment.claimSearchHost + requestURL);
     return this.http.request(request);
   }
 
-  downloadSummaries(providerId:string, fromDate:string, toDate:string, payerId:string, status:string){
-    const requestURL:string = '/'+providerId+'/claim-download?fromDate='+fromDate
-    +'&toDate='+toDate+'&payerId='+payerId+'&status='+status;
-    const request = new HttpRequest('GET', environment.claimSearchHost+requestURL, "", {responseType: "text"});
+  downloadSummaries(providerId: string, fromDate: string, toDate: string, payerId: string, status: string) {
+    const requestURL: string = '/' + providerId + '/claim-download?fromDate=' + fromDate
+      + '&toDate=' + toDate + '&payerId=' + payerId + '&status=' + status;
+    const request = new HttpRequest('GET', environment.claimSearchHost + requestURL, "", { responseType: "text" });
     return this.http.request(request);
   }
 
-  getClaim(providerId:string, claimId:string){
+  getClaim(providerId: string, claimId: string) {
     const requestURL = `/${providerId}/claims/${claimId}`;
-    const request = new HttpRequest('GET', environment.claimSearchHost+requestURL);
+    const request = new HttpRequest('GET', environment.claimSearchHost + requestURL);
     return this.http.request(request);
   }
 }
