f79d1dff34196351328b4e3efbaff993d3b4a33b
Anas Dawood <anas_dawood@hotmail.com>
2020-01-01 12:06:15 +0300

    - implementing payment reference details page

--- src/app/components/payment-reference-report/payment-reference-report.component.ts
+++ src/app/components/payment-reference-report/payment-reference-report.component.ts
@@ -4,6 +4,9 @@ import { CommenServicesService } from 'src/app/services/commen-services.service'
 import { ActivatedRoute, Router } from '@angular/router';
 import { MatPaginator } from '@angular/material';
 import { HttpResponse, HttpErrorResponse, HttpEventType } from '@angular/common/http';
+import { PaymentRefernceDetail } from 'src/app/models/paymentRefernceDetail';
+import { PaginatedResult } from 'src/app/models/paginatedResult';
+import { SearchStatusSummary } from 'src/app/models/searchStatusSummary';
 
 @Component({
   selector: 'app-payment-reference-report',
@@ -12,9 +15,10 @@ import { HttpResponse, HttpErrorResponse, HttpEventType } from '@angular/common/
 })
 export class PaymentReferenceReportComponent implements OnInit {
 
-  detailCardTitle: string;
+  summaries: SearchStatusSummary[];
+  detailCardTitle = "Payment References";
   detailTopActionText = "vertical_align_bottom";
-  detailAccentColor: string;
+  detailAccentColor = "#3060AA";
   detailActionText: string = null;
   detailSubActionText: string = null;
   detailCheckBoxIndeterminate: boolean;
@@ -32,14 +36,21 @@ export class PaymentReferenceReportComponent implements OnInit {
 
   errorMessage: string;
 
+  paymentDetails: PaginatedResult<PaymentRefernceDetail>;
+  payments = Array();
 
-  constructor(public reportService:ReportsService,public commen:CommenServicesService, public routeActive: ActivatedRoute, public router: Router) { }
+
+  constructor(public reportService: ReportsService, public commen: CommenServicesService, public routeActive: ActivatedRoute, public router: Router) { }
 
   ngOnInit() {
-    this.fetchData()
+    this.fetchData();
   }
-  async fetchData()
-  {
+  async fetchData() {
+
+    this.summaries = new Array();
+    this.commen.loadingChanged.next(true);
+    this.errorMessage = null;
+
     this.routeActive.params.subscribe(value => {
       this.providerId = value.providerId;
     });
@@ -51,10 +62,16 @@ export class PaymentReferenceReportComponent implements OnInit {
       if (Number.isNaN(this.queryPage) || this.queryPage < 0) this.queryPage = 0;
     });
     let event;
-    event = await this.reportService.getPaymentSummary(this.providerId, this.from, this.to, this.payerId,this.queryPage,this.queryPage).subscribe((event) => {
+    event = await this.reportService.getPaymentSummary(this.providerId, this.from, this.to, this.payerId, this.queryPage, this.queryPage).subscribe((event) => {
       if (event instanceof HttpResponse) {
         if ((event.status / 100).toFixed() == "2") {
-         console.log(event);
+          this.paymentDetails = new PaginatedResult(event.body, PaymentRefernceDetail);
+          this.payments = this.paymentDetails.content;
+          console.log(event.body);
+          console.log(this.paymentDetails);
+          const pages = Math.ceil((this.paymentDetails.totalElements / this.paginator.pageSize));
+          this.paginatorPagesNumbers = Array(pages).fill(pages).map((x, i) => i);
+          this.manualPage = this.paginator.pageIndex;
         } else if ((event.status / 100).toFixed() == "4") {
           console.log("400");
         } else if ((event.status / 100).toFixed() == "5") {
@@ -76,6 +93,35 @@ export class PaymentReferenceReportComponent implements OnInit {
       }
       this.commen.loadingChanged.next(false);
     });
+  }
+
+  paginatorAction(event) {
+    this.manualPage = event['pageIndex'];
+  }
+  updateManualPage(index) {
+    this.manualPage = index;
+    this.paginator.pageIndex = index;
+    this.paginatorAction({ previousPageIndex: this.paginator.pageIndex, pageIndex: index, pageSize: this.paginator.pageSize, length: this.paginator.length })
+  }
+
+  get paginatorLength() {
+    if (this.paymentDetails != null) {
+      return this.paymentDetails.totalElements;
+    }
+    else return 0;
+  }
+
+  mapPayer(payerId:string)
+  {
+
+    switch(payerId) { 
+      case "300": { 
+        return "MedGulf"; 
+      } 
+      default: { 
+         return "";
+      } 
+   } 
   }
 
 }
