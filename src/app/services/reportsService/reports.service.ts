32c585ad2ca925f80b77bb725fadd3e3af481b99
Anas Dawood <anas_dawood@hotmail.com>
2020-01-07 14:36:05 +0300

    Download Submitted Invoices

--- src/app/services/reportsService/reports.service.ts
+++ src/app/services/reportsService/reports.service.ts
@@ -50,15 +50,13 @@ export class ReportsService {
     return this.http.request(request);
   }
 
-  // downloadSubmittedInvoices(providerId: string, status: string, fromDate?: string, toDate?: string, payerId?: string, batchId?: string) {
-  //   let requestURL: string = '/' + providerId + '/claim-download?status=' + status;
-  //   if(fromDate != null && toDate != null && payerId != null){
-  //     requestURL += `&fromDate=${fromDate}&toDate=${toDate}&payerId=${payerId}`;
-  //   } else{
-  //     requestURL += `&batchId=${batchId}`;
-  //   }
-  //   const request = new HttpRequest('GET', environment.claimSearchHost + requestURL, "", { responseType: "text" });
-  //   return this.http.request(request);
-  // }
+  downloadSubmittedInvoices(providerId: string, fromDate?: string, toDate?: string, payerId?: string) {
+    let requestURL: string = '/' + providerId + '/claim/paid/csv?' + status;
+    
+      requestURL += `fromDate=${fromDate}&toDate=${toDate}&payerId=${payerId}`;
+    
+    const request = new HttpRequest('GET', environment.claimSearchHost + requestURL, "", { responseType: "text" });
+    return this.http.request(request);
+  }
 
 }
