1222179f4ef44a90b9f2756266a6fc33d0fa3949
Ahmed.Alyousef <ahmed.alyousef@waseel.com>
2020-02-20 18:30:41 +0300

    change qa hosts to use gateway

--- src/environments/environment.qa.ts
+++ src/environments/environment.qa.ts
@@ -1,10 +1,10 @@
 export const environment = {
     production: true,
-    uploaderHost: "http://claim-uploader-waseele-cloud.apps.okd.waseel.com",
-    claimSearchHost: "http://claim-search-waseele-cloud.apps.okd.waseel.com",
-    claimServiceHost: "	http://claim-service-waseele-cloud.apps.okd.waseel.com",
-    NotificationServiceHost: "http://notification-service-waseele-cloud.apps.okd.waseel.com",
-    authenticationHost: "	http://authentication-waseele-cloud.apps.okd.waseel.com",
-    adminServiceHost: "http://admin-service-waseele-cloud.apps.okd.waseel.com",
-    auditTrailServiceHost: "http://audit-trail-waseele-cloud.apps.okd.waseel.com"
+    uploaderHost: "http://gateway.okd.waseel.com/claim-uploader",
+    claimSearchHost: "http://gateway.okd.waseel.com/claim-search",
+    claimServiceHost: "http://gateway.okd.waseel.com/claim-service",
+    NotificationServiceHost: "http://gateway.okd.waseel.com/notification-service",
+    authenticationHost: "http://gateway.okd.waseel.com/authentication",
+    adminServiceHost: "http://gateway.okd.waseel.com/admin-service",
+    auditTrailServiceHost: "http://gateway.okd.waseel.com/audit-trail"
   };
