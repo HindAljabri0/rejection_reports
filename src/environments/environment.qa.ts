4a64a708faa4119d8104d13d4f93a24e00e5a2d0
Arslan Masood <arslan.masood@waseel.com>
2020-02-23 12:29:30 +0300

    Update environment.qa.ts

--- src/environments/environment.qa.ts
+++ src/environments/environment.qa.ts
@@ -1,10 +1,10 @@
 export const environment = {
     production: true,
-    uploaderHost: "http://gateway.okd.waseel.com/claim-uploader",
-    claimSearchHost: "http://gateway.okd.waseel.com/claim-search",
-    claimServiceHost: "http://gateway.okd.waseel.com/claim-service",
-    NotificationServiceHost: "http://gateway.okd.waseel.com/notification-service",
-    authenticationHost: "http://gateway.okd.waseel.com/authentication",
-    adminServiceHost: "http://gateway.okd.waseel.com/admin-service",
-    auditTrailServiceHost: "http://gateway.okd.waseel.com/audit-trail"
+    uploaderHost: "http://gateway.okd.waseel.com/api/claim-uploader",
+    claimSearchHost: "http://gateway.okd.waseel.com/api/claim-search",
+    claimServiceHost: "http://gateway.okd.waseel.com/api/claim-service",
+    NotificationServiceHost: "http://gateway.okd.waseel.com/api/notification-service",
+    authenticationHost: "http://gateway.okd.waseel.com/api/authentication",
+    adminServiceHost: "http://gateway.okd.waseel.com/api/admin-service",
+    auditTrailServiceHost: "http://gateway.okd.waseel.com/api/audit-trail"
   };
