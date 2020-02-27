e64b556cc79b8069cd4dd8952cd6bb417285c79b
Ahmed.Alyousef <ahmed.alyousef@waseel.com>
2020-02-27 14:42:10 +0300

    create staging environment

--- src/environments/environment.qa.ts
+++ src/environments/environment.qa.ts
@@ -1,10 +1,11 @@
+const host = 'http://gateway.okd.waseel.com/api';
 export const environment = {
     production: true,
-    uploaderHost: "http://gateway.okd.waseel.com/api/claim-uploader",
-    claimSearchHost: "http://gateway.okd.waseel.com/api/claim-search",
-    claimServiceHost: "http://gateway.okd.waseel.com/api/claim-service",
-    NotificationServiceHost: "http://gateway.okd.waseel.com/api/notification-service",
-    authenticationHost: "http://gateway.okd.waseel.com/api/authentication",
-    adminServiceHost: "http://gateway.okd.waseel.com/api/admin-service",
-    auditTrailServiceHost: "http://gateway.okd.waseel.com/api/audit-trail"
+    uploaderHost: `${host}/claim-uploader`,
+    claimSearchHost: `${host}/claim-search`,
+    claimServiceHost: `${host}/claim-service`,
+    NotificationServiceHost: `${host}/notification-service`,
+    authenticationHost: `${host}/authentication`,
+    adminServiceHost: `${host}/admin-service`,
+    auditTrailServiceHost: `${host}/audit-trail`
   };
---
+++ src/environments/environment.staging.ts
@@ -0,0 +1,11 @@
+const host = 'http://api.stg-eclaims.waseel.com';
+export const environment = {
+    production: true,
+    uploaderHost: `${host}/upload`,
+    claimSearchHost: `${host}/search`,
+    claimServiceHost: `${host}/claims`,
+    NotificationServiceHost: `${host}/notifications`,
+    authenticationHost: `${host}/oauth`,
+    adminServiceHost: `${host}/admin`,
+    auditTrailServiceHost: `${host}/audit`
+  };
