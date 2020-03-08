ee904bea3b603639f13a495a41bad815a5735df7
Ahmed.Alyousef <ahmed.alyousef@waseel.com>
2020-03-08 12:48:27 +0300

    edit notifications url

--- src/app/services/notificationService/notifications.service.ts
+++ src/app/services/notificationService/notifications.service.ts
@@ -10,19 +10,19 @@ export class NotificationsService {
   constructor(private httpClient:HttpClient) { }
 
   getNotifications(providerId:string, page:number, pageSize:number){
-    const requestUrl = `/${providerId}/notifications?page=${page}&size=${pageSize}`;
+    const requestUrl = `/providers/${providerId}?page=${page}&size=${pageSize}`;
     const request = new HttpRequest('GET', environment.NotificationServiceHost+requestUrl);
     return this.httpClient.request(request);
   }
 
   getNotificationsCount(providerId:string, status:string){
-    const requestUrl = `/${providerId}/notifications/${status}/count`;
+    const requestUrl = `/providers/${providerId}/count/${status}`;
     const request = new HttpRequest('GET', environment.NotificationServiceHost+requestUrl);
     return this.httpClient.request(request);
   }
 
   markNotificationAsRead(providerId:string, id:string){
-    const requestUrl = `/${providerId}/notifications/${id}`;
+    const requestUrl = `/providers/${providerId}/${id}`;
     const request = new HttpRequest('PUT', environment.NotificationServiceHost+requestUrl,{});
     return this.httpClient.request(request);
   }
