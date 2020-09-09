44662fbba980d5f1a7e393e5fe7776bde83d82dc
Ahmed Al Yousif <ahmadalasiddeeq@gmail.com>
2020-09-09 12:34:49 +0300

    add environment files for oci

---
+++ src/environments/environment.oci_qa.ts
@@ -0,0 +1,13 @@
+const host = 'http://gateway.okd.waseel.com';
+export const environment = {
+  production: true,
+  uploaderHost: `${host}/claim-uploader`,
+  claimSearchHost: `${host}/claim-search`,
+  claimServiceHost: `${host}/claim-service`,
+  NotificationServiceHost: `${host}/notification-service`,
+  authenticationHost: `${host}/authentication`,
+  adminServiceHost: `${host}/admin-service`,
+  auditTrailServiceHost: `${host}/audit-trail`,
+  settingsServiceHost: `${host}/settings`,
+  claimInquireServiceHost: `${host}/claim-inquiry`
+};
