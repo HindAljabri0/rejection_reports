e7cb7d1d05dc6e722dbfa8fc6aa3e3ff3544cb9e
Anas Dawood <anas_dawood@hotmail.com>
2020-10-05 11:02:22 +0300

    - remove encodeURI from front end

--- src/app/pages/searchClaimsPage/search-claims.component.ts
+++ src/app/pages/searchClaimsPage/search-claims.component.ts
@@ -648,7 +648,7 @@ export class SearchClaimsComponent implements OnInit, AfterViewChecked, OnDestro
       } else {
         var a = document.createElement("a");
         var excelData = event.body;
-        a.href = 'data:attachment/csv;charset=ISO-8859-1,' + encodeURI( excelData + "");
+        a.href = 'data:attachment/csv;charset=ISO-8859-1,' + excelData ;
         a.target = '_blank';
         if (this.from != null) {
           a.download = this.detailCardTitle + '_' + this.from + '_' + this.to + '.csv';
