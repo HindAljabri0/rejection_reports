ba6a5faea92a1c5f663177c981fd8df650367cdc
Ahmed Al Yousif <ahmadalasiddeeq@gmail.com>
2020-11-19 11:04:39 +0300

    start of code value management

--- src/app/modules/app-routing.module.ts
+++ src/app/modules/app-routing.module.ts
@@ -9,35 +9,37 @@ import { LoginComponent } from '../pages/loginpage/login.component';
 import { ClaimpageComponent } from '../pages/claimUploadignPage/claimpage.component';
 import { UploadsHistoryComponent } from '../pages/uploads-history/uploads-history.component';
 import { ReportsComponent } from '../pages/reports/reports-page.component';
+import { ConfigurationsComponent } from '../pages/configurations/configurations.component';
 
 
 
 @NgModule({
-    imports: [
-        RouterModule.forRoot([
-            { path: '', component: DashboardComponent, canActivate: [RouteCanActiveService] },
-            { path: ':providerId/claims', component: SearchClaimsComponent, canActivate: [RouteCanActiveService] },
-            { path: ':providerId/notifications', component: NotificationsPageComponent, canActivate: [RouteCanActiveService] },
-            { path: ':providerId/announcements', component: AnnouncementsPageComponent, canActivate: [RouteCanActiveService] },
-            { path: 'login', component: LoginComponent },
-            { path: 'upload', component: ClaimpageComponent, canActivate: [RouteCanActiveService] },
-            { path: 'upload/history', component: UploadsHistoryComponent, canActivate: [RouteCanActiveService] },
-            { path: 'summary', component: ClaimpageComponent, canActivate: [RouteCanActiveService] },
-            { path: ':providerId/reports', component: ReportsComponent, canActivate: [RouteCanActiveService] },
-            {
-              path: 'administration',
-              loadChildren: () => import('./adminstration/adminstration.module').then(m => m.AdminstrationModule),
-              canLoad: [RouteCanActiveService]
-            },
-            {
-              path: 'claims',
-              loadChildren: () => import('./claim/claim.module').then(m => m.ClaimModule),
-              canLoad: [RouteCanActiveService]
-            }
-          ])
-    ],
-    exports: [
-        RouterModule
-    ]
+  imports: [
+    RouterModule.forRoot([
+      { path: '', component: DashboardComponent, canActivate: [RouteCanActiveService] },
+      { path: ':providerId/claims', component: SearchClaimsComponent, canActivate: [RouteCanActiveService] },
+      { path: ':providerId/notifications', component: NotificationsPageComponent, canActivate: [RouteCanActiveService] },
+      { path: ':providerId/announcements', component: AnnouncementsPageComponent, canActivate: [RouteCanActiveService] },
+      { path: 'login', component: LoginComponent },
+      { path: 'upload', component: ClaimpageComponent, canActivate: [RouteCanActiveService] },
+      { path: 'upload/history', component: UploadsHistoryComponent, canActivate: [RouteCanActiveService] },
+      { path: 'summary', component: ClaimpageComponent, canActivate: [RouteCanActiveService] },
+      { path: ':providerId/reports', component: ReportsComponent, canActivate: [RouteCanActiveService] },
+      { path: 'configurations', component: ConfigurationsComponent, canActivate: [RouteCanActiveService] },
+      {
+        path: 'administration',
+        loadChildren: () => import('./adminstration/adminstration.module').then(m => m.AdminstrationModule),
+        canLoad: [RouteCanActiveService]
+      },
+      {
+        path: 'claims',
+        loadChildren: () => import('./claim/claim.module').then(m => m.ClaimModule),
+        canLoad: [RouteCanActiveService]
+      }
+    ])
+  ],
+  exports: [
+    RouterModule
+  ]
 })
 export class AppRoutingModule { }
