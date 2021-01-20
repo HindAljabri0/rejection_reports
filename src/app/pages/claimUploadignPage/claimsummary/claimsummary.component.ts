80f407f44974d64ad35448671594894d4c379c83
Akshat Mewada <akshat.mewada@tatvasoft.com>
2021-01-20 15:57:21 +0530

    Added carousel in upload claims and upload summary page

--- src/app/pages/claimUploadignPage/claimsummary/claimsummary.component.ts
+++ src/app/pages/claimUploadignPage/claimsummary/claimsummary.component.ts
@@ -16,6 +16,7 @@ import { ClaimfileuploadComponent } from '../claimfileupload/claimfileupload.com
 import { DialogService } from 'src/app/services/dialogsService/dialog.service';
 import { MessageDialogData } from 'src/app/models/dialogData/messageDialogData';
 import { ClaimService } from 'src/app/services/claimService/claim.service';
+import { OwlOptions } from 'ngx-owl-carousel-o';
 
 
 
@@ -25,7 +26,29 @@ import { ClaimService } from 'src/app/services/claimService/claim.service';
   styles: []
 })
 export class ClaimsummaryComponent implements OnInit, OnDestroy {
-
+  owlCarouselOptions: OwlOptions = {
+    mouseDrag: false,
+    pullDrag: false,
+    dots: false,
+    navSpeed: 300,
+    navText: ['', ''],
+    margin: 14,
+    responsive: {
+      0: {
+        items: 3,
+        slideBy: 3
+      },
+      992: {
+        items: 4,
+        slideBy: 4
+      },
+      1200: {
+        items: 5,
+        slideBy: 5
+      }
+    },
+    nav: true
+  };
 
 
   constructor(public location: Location, public uploadService: UploadService, public commen: SharedServices, private router: Router,
