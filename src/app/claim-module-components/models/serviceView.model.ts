f5a4e7fbb387c0f7bb1b27ade523fa1c5b5138ab
Ahmed Al Yousif <ahmadalasiddeeq@gmail.com>
2021-01-28 17:40:34 +0300

    show service description

---
+++ src/app/claim-module-components/models/serviceView.model.ts
@@ -0,0 +1,17 @@
+import { Service } from "./service.model";
+import { GDPN } from "./GDPN.model";
+
+
+export class ServiceView extends Service {
+
+    serviceDecision: {
+        invoiceNumber: string;
+        serviceNumber: number;
+        serviceStatusCode: string;
+        serviceStatusDescription: string;
+        gdpn: GDPN;
+        approvedQuantity: number;
+        serviceDenialCode: string;
+        decisionComment: string;
+    }
+}
