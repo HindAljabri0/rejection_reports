930a2c13c6aee701a0deb81cd945c104ce371b2e
Tejas Bhatt <tejas.bhatt@tatvasoft.com>
2021-02-12 14:59:57 +0530

    empty state when no records found

---
+++ src/app/components/reusables/empty-state/empty-state.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-empty-state',
+  templateUrl: './empty-state.component.html',
+  styleUrls: ['./empty-state.component.css']
+})
+export class EmptyStateComponent implements OnInit {
+
+  @Input() message = 'No data found !';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
