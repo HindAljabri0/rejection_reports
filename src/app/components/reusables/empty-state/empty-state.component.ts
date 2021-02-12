5642a8ecf559339d2907cf4003c0771faa9d2072
Akshat Mewada <akshat.mewada@tatvasoft.com>
2021-02-12 17:43:36 +0530

    Modified empty states

--- src/app/components/reusables/empty-state/empty-state.component.ts
+++ src/app/components/reusables/empty-state/empty-state.component.ts
@@ -3,11 +3,11 @@ import { Component, OnInit, Input } from '@angular/core';
 @Component({
   selector: 'app-empty-state',
   templateUrl: './empty-state.component.html',
-  styleUrls: ['./empty-state.component.css']
+  styles: []
 })
 export class EmptyStateComponent implements OnInit {
 
-  @Input() message = 'No data found !';
+  @Input() message = 'No data found!';
 
   constructor() { }
 
