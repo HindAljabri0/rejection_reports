56a23247f4d07780ba581401bfa2de886fc01a19
Tejas Bhatt <tejas.bhatt@tatvasoft.com>
2021-05-10 17:13:26 +0530

    Resolve the issue of the lable and add the condtion at the PBM Labels.

--- src/app/claim-module-components/invoices-services/invoices-services.component.ts
+++ src/app/claim-module-components/invoices-services/invoices-services.component.ts
@@ -513,8 +513,8 @@ export class InvoicesServicesComponent implements OnInit {
       requestedQuantity: service.quantity.value,
       toothNumber: service.toothNumber.value,
       daysOfSupply: service.daysOfSupply.value,
-      pbmServiceError: service.pbmServiceError.value,
-      pbmServiceStatus: service.pbmServiceStatus.value,
+      pbmServiceError: service.pbmServiceError,
+      pbmServiceStatus: service.pbmServiceStatus,
       serviceGDPN: {
         patientShare: { value: service.patientShare.value, type: 'SAR' },
         discount: { value: service.serviceDiscount.value, type: service.serviceDiscountUnit },
