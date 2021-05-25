7c6a2cdd6af74d277c3d016e7a2817d6eb116c11
Tejas Bhatt <tejas.bhatt@tatvasoft.com>
2021-05-25 11:45:25 +0530

    Implemented code to API call to generate graph in the revenu-tracking-report page

--- src/app/services/revenuReportService/revenu-report.service.ts
+++ src/app/services/revenuReportService/revenu-report.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { generateCleanClaimProgressReport } from 'src/app/models/generateCleanClaimProgressReport';
 import { Observable } from 'rxjs';
-import { HttpRequest, HttpClient } from '@angular/common/http';
+import { HttpRequest, HttpClient, HttpParams } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { RevenuTrackingReport } from 'src/app/models/revenuReportTrackingReport';
 import { RevenuComparativeReport } from 'src/app/models/revenuComparativeReport';
@@ -13,8 +13,15 @@ export class RevenuReportService {
 
   constructor(private http: HttpClient) { }
   generateRevenuTrackingReport(providerId: string, data: RevenuTrackingReport): Observable<any> {
-    const requestURL = `/providers/${providerId}/charts`;
-    const request = new HttpRequest('POST', environment.uploaderHost + requestURL, data, { responseType: 'text' });
+    const requestURL = `/providers/${providerId}/revenue-tracking/${data.subcategory}`;
+
+    let searchparams = new HttpParams();
+    if (data) {
+      for (const key in data) {
+        if (data.hasOwnProperty(key) && data[key] !== undefined && key !== 'subcategory') { searchparams = searchparams.set(key, data[key]); }
+      }
+    }
+    const request = new HttpRequest('GET', environment.claimSearchHost + requestURL, { responseType: 'text', params: searchparams });
     return this.http.request(request);
   }
   generateRevenuComparativeProgressReport(providerId: string, data: RevenuComparativeReport): Observable<any> {
