45104e7abf57e395cbdc51f8c13d660f12f1c5c3
Akshat Mewada <akshat.mewada@tatvasoft.com>
2021-06-29 11:25:47 +0530

    Added provider contract page

--- src/app/modules/adminstration/adminstration.module.ts
+++ src/app/modules/adminstration/adminstration.module.ts
@@ -3,18 +3,23 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { NgScrollbarModule, SmoothScrollModule } from 'ngx-scrollbar';
 import { AuditTrailComponent } from 'src/app/pages/audit-trail/audit-trail.component';
 import { ProviderMappingComponent } from 'src/app/pages/provider-mapping/provider-mapping.component';
 import { ProvidersConfigComponent } from 'src/app/pages/providers-config/providers-config.component';
 import { RequestInterceptorService } from 'src/app/services/RequestInterceptorService/request-interceptor.service';
 import { MaterialModule } from '../material/material.module';
 import { SharedModule } from '../shared.module';
+import { ProviderContractComponent } from './provider-contract/provider-contract.component';
+import { AddProviderContractDialogComponent } from './add-provider-contract-dialog/add-provider-contract-dialog.component';
 
 @NgModule({
   declarations: [
     AuditTrailComponent,
     ProvidersConfigComponent,
-    ProviderMappingComponent
+    ProviderMappingComponent,
+    ProviderContractComponent,
+    AddProviderContractDialogComponent
   ],
   imports: [
     RouterModule.forChild([
@@ -22,7 +27,8 @@ import { SharedModule } from '../shared.module';
       { path: 'auditLogs', component: AuditTrailComponent },
       { path: 'config/providers', component: ProvidersConfigComponent },
       { path: 'config/providerMapping', component: ProviderMappingComponent },
-      { path: 'config/providers/:providerId', component: ProvidersConfigComponent }
+      { path: 'config/providers/:providerId', component: ProvidersConfigComponent },
+      { path: 'config/provider-contract', component: ProviderContractComponent }
     ]),
     CommonModule,
     MaterialModule,
@@ -30,9 +36,14 @@ import { SharedModule } from '../shared.module';
     SharedModule,
     HttpClientModule,
     FormsModule,
+    NgScrollbarModule,
+    SmoothScrollModule,
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: RequestInterceptorService, multi: true },
   ],
+  entryComponents: [
+    AddProviderContractDialogComponent
+  ]
 })
 export class AdminstrationModule { }
