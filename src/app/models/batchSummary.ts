2480d2a0a6c2fc7410b74e02ecfbaf0e2b635db9
Tejas Bhatt <tejas.bhatt@tatvasoft.com>
2021-07-07 16:04:05 +0530

    Implemented code to API call to fetch claims with No Batch based on search criteria in create-batch page

---
+++ src/app/models/batchSummary.ts
@@ -0,0 +1,26 @@
+import { Paginateable } from './paginateable';
+
+export class BatchSummaryModel extends Paginateable {
+    approvalnumber: string;
+    memberid: string;
+    idnumber: string;
+    patientfilenumber: string;
+    visitdate: string;
+    net: number;
+    netvatamount: number;
+    claimid: number;
+    physicianname: string;
+
+    constructor(body: {}) {
+        super(body);
+        this.approvalnumber = body['approvalnumber'];
+        this.memberid = body['memberid'];
+        this.idnumber = body['idnumber'];
+        this.patientfilenumber = body['patientfilenumber'];
+        this.visitdate = body['visitdate'];
+        this.net = body['net'];
+        this.netvatamount = body['netvatamount'];
+        this.claimid = body['claimid'];
+        this.physicianname = body['physicianname'];
+    }
+}
---
+++ src/app/models/batchSummaryModel.ts
@@ -0,0 +1,8 @@
+export class BatchSummary {
+    payerId: any = '0';
+    startDate: any = '';
+    endDate: any = '';
+    page: number;
+    pageSize: number;
+    totalPages: number;
+}
