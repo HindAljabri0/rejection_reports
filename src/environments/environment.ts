8fef848f061b604cd8d0aa88189f6b1722a80cac
Akshat Mewada <akshat.mewada@tatvasoft.com>
2021-07-20 13:15:35 +0530

    env changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,26 +2,26 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-// export const environment = {
-//   name: 'dev',
-//   production: false,
-//   versionCheckURL: `http://localhost:4200`,
-//   uploaderHost: 'http://localhost:8080',
-//   claimSearchHost: 'http://localhost:8082',
-//   claimServiceHost: 'http://localhost:8081',
-//   NotificationServiceHost: 'http://localhost:8222/notification-service',
-//   authenticationHost: 'http://localhost:8086',
-//   adminServiceHost: 'http://localhost:8087',
-//   auditTrailServiceHost: 'http://localhost:8089',
-//   settingsServiceHost: 'http://localhost:8111',
-//   claimInquireServiceHost: 'http://localhost:8580',
-//   validationServiceHost: 'http://localhost:8121',
-//   creditReportService: 'http://localhost:8484',
-//   tawuniyaCreditReportService: 'http://localhost:8491',
-//   pbmValidationService: 'http://localhost:8099',
-//   payerPaymentContractService: `http://localhost:8089/payer-payment-contract`
+export const environment = {
+  name: 'dev',
+  production: false,
+  versionCheckURL: `http://localhost:4200`,
+  uploaderHost: 'http://localhost:8080',
+  claimSearchHost: 'http://localhost:8082',
+  claimServiceHost: 'http://localhost:8081',
+  NotificationServiceHost: 'http://localhost:8222/notification-service',
+  authenticationHost: 'http://localhost:8086',
+  adminServiceHost: 'http://localhost:8087',
+  auditTrailServiceHost: 'http://localhost:8089',
+  settingsServiceHost: 'http://localhost:8111',
+  claimInquireServiceHost: 'http://localhost:8580',
+  validationServiceHost: 'http://localhost:8121',
+  creditReportService: 'http://localhost:8484',
+  tawuniyaCreditReportService: 'http://localhost:8491',
+  pbmValidationService: 'http://localhost:8099',
+  payerPaymentContractService: `http://localhost:8089/payer-payment-contract`
 
-// };
+};
 
 /*
  * For easier debugging in development mode, you can import the following file
@@ -31,24 +31,3 @@
  * on performance if an error is thrown.
  */
 // import 'zone.js/dist/zone-error';  // Included with Angular CLI.
-
-
-export const environment = {
-  name: 'dev',
-  production: false,
-  versionCheckURL: `http://192.168.1.135:4200`,
-  uploaderHost: 'http://192.168.1.135:8080',
-  claimSearchHost: 'http://192.168.1.135:8082',
-  claimServiceHost: 'http://192.168.1.135:8081',
-  NotificationServiceHost: 'http://localhost:8222/notification-service',
-  authenticationHost: ' https://api.qa-eclaims.waseel.com/oauth/',
-  adminServiceHost: 'http://192.168.1.135:8087',
-  auditTrailServiceHost: 'http://192.168.1.135:8089',
-  settingsServiceHost: 'http://192.168.1.135:8111',
-  claimInquireServiceHost: 'http://192.168.1.135:8580',
-  validationServiceHost: 'http://192.168.1.135:8121',
-  creditReportService: 'http://192.168.1.135:8484',
-  tawuniyaCreditReportService: 'http://192.168.1.135:8491',
-  pbmValidationService: 'http://192.168.0.135:8099',
-  payerPaymentContractService: `http://192.168.1.135:8089/payer-payment-contract`
-};
