6ad87d9ce52c51e3cefaf8d40a3f66e3834fa47c
Akshat Mewada <akshat.mewada@tatvasoft.com>
2021-07-22 13:58:33 +0530

    Modified design for XML viewer

--- src/app/components/dialogs/xml-view-dialog/xml-view-dialog.component.ts
+++ src/app/components/dialogs/xml-view-dialog/xml-view-dialog.component.ts
@@ -4,21 +4,25 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 @Component({
   selector: 'app-xml-view-dialog',
   templateUrl: './xml-view-dialog.component.html',
-  styleUrls: ['./xml-view-dialog.component.css']
+  styles: []
 })
 export class XmlViewDialogComponent implements OnInit {
   xmlData: any;
 
-  constructor(private dialogRef: MatDialogRef<XmlViewDialogComponent>,
+  constructor(
+    private dialogRef: MatDialogRef<XmlViewDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any) { }
   editorOptions = { theme: '', language: 'xml', readOnly: true };
   ngOnInit() {
     this.xmlData = this.data.mainData.submittedData;
-
   }
   onInit(editor) {
-    let line = editor.getPosition();
+    const line = editor.getPosition();
     console.log(line);
+  }
+
+  closeDialog() {
+    this.dialogRef.close();
   }
 
 }
