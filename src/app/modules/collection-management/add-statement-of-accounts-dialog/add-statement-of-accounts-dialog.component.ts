b8c165ffa434acc70589c89e33a0fb31ddf8d262
Tejas Bhatt <tejas.bhatt@tatvasoft.com>
2021-07-28 11:10:36 +0530

    Implemented code to Search Criteria and add statment valiation in the SOA pages.

--- src/app/modules/collection-management/add-statement-of-accounts-dialog/add-statement-of-accounts-dialog.component.ts
+++ src/app/modules/collection-management/add-statement-of-accounts-dialog/add-statement-of-accounts-dialog.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialogRef } from '@angular/material';
-
+import { BsDatepickerConfig } from 'ngx-bootstrap';
+import { AddStatmentAccountModel } from 'src/app/models/statementAccountModel';
+import * as moment from 'moment';
+import { SharedServices } from 'src/app/services/shared.services';
 @Component({
   selector: 'app-add-statement-of-accounts-dialog',
   templateUrl: './add-statement-of-accounts-dialog.component.html',
@@ -8,8 +11,13 @@ import { MatDialogRef } from '@angular/material';
 })
 export class AddStatementOfAccountsDialogComponent implements OnInit {
   fileUploadFlag = false;
-
-  constructor(private dialogRef: MatDialogRef<AddStatementOfAccountsDialogComponent>) { }
+  minDate: any;
+  datePickerConfig: Partial<BsDatepickerConfig> = { showWeekNumbers: false, dateInputFormat: 'DD/MM/YYYY' };
+  addStatmentAccountModel = new AddStatmentAccountModel();
+  currentFileUpload: File;
+  fileError = '';
+  sizeInMB: string;
+  constructor(private dialogRef: MatDialogRef<AddStatementOfAccountsDialogComponent>, public common: SharedServices) { }
 
   ngOnInit() {
   }
@@ -18,8 +26,48 @@ export class AddStatementOfAccountsDialogComponent implements OnInit {
     this.dialogRef.close();
   }
 
-  fileUploadChange() {
+  dateValidation(event: any) {
+    if (event !== null) {
+      const startDate = moment(event).format('YYYY-MM-DD');
+      const endDate = moment(this.addStatmentAccountModel.statmentEndDate).format('YYYY-MM-DD');
+      if (startDate > endDate) {
+        this.addStatmentAccountModel.statmentEndDate = '';
+      }
+    }
+    this.minDate = new Date(event);
+
+  }
+  checkfile() {
+    const validExts = new Array('.csv');
+    let fileExt = this.currentFileUpload.name;
+    fileExt = fileExt.substring(fileExt.lastIndexOf('.'));
+    if (validExts.indexOf(fileExt) < 0) {
+      this.showError('Invalid file selected, valid files are of ' +
+        validExts.toString() + ' types.');
+      return false;
+    } else {
+      this.fileError = '';
+      return true;
+    }
+  }
+  showError(error: string) {
+    this.currentFileUpload = null;
+    this.fileError = error;
+    this.common.loadingChanged.next(false);
+  }
+
+  submit() {
+
+  }
+  selectFile(event) {
     this.fileUploadFlag = true;
+    this.currentFileUpload = event.target.files[0];
+    this.sizeInMB = this.common.formatBytes(this.currentFileUpload.size);
+    if (!this.checkfile()) {
+      event.target.files = [];
+      this.currentFileUpload = undefined;
+      return;
+    }
   }
 
 }
--- src/app/modules/collection-management/statement-of-accounts/statement-of-accounts.component.ts
+++ src/app/modules/collection-management/statement-of-accounts/statement-of-accounts.component.ts
@@ -1,14 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material';
 import { AddStatementOfAccountsDialogComponent } from '../add-statement-of-accounts-dialog/add-statement-of-accounts-dialog.component';
-
+import { StatementAccountSummary } from 'src/app/models/statementAccountModel';
+import * as moment from 'moment';
+import { BsDatepickerConfig } from 'ngx-bootstrap';
 @Component({
   selector: 'app-statement-of-accounts',
   templateUrl: './statement-of-accounts.component.html',
   styles: []
 })
 export class StatementOfAccountsComponent implements OnInit {
-
+  stamentAccountModel = new StatementAccountSummary();
+  minDate: any;
+  datePickerConfig: Partial<BsDatepickerConfig> = { showWeekNumbers: false, dateInputFormat: 'DD/MM/YYYY' };
   constructor(public dialog: MatDialog) { }
 
   ngOnInit() {
@@ -19,5 +23,42 @@ export class StatementOfAccountsComponent implements OnInit {
       panelClass: ['primary-dialog']
     });
   }
+  submit() {
 
+  }
+
+  goToFirstPage() {
+    if (this.stamentAccountModel.pageNo != 0) {
+      this.stamentAccountModel.pageNo = 0;
+    }
+  }
+  goToPrePage() {
+    if (this.stamentAccountModel.pageNo != 0) {
+      this.stamentAccountModel.pageNo = this.stamentAccountModel.pageNo - 1;
+    }
+  }
+  goToNextPage() {
+    if ((this.stamentAccountModel.pageNo + 1) < this.stamentAccountModel.totalPages) {
+      this.stamentAccountModel.pageNo = this.stamentAccountModel.pageNo + 1;
+
+
+    }
+  }
+  goToLastPage() {
+    if (this.stamentAccountModel.pageNo != (this.stamentAccountModel.totalPages - 1)) {
+      this.stamentAccountModel.pageNo = this.stamentAccountModel.totalPages - 1;
+
+    }
+  }
+  dateValidation(event: any) {
+    if (event !== null) {
+      const startDate = moment(event).format('YYYY-MM-DD');
+      const endDate = moment(this.stamentAccountModel.endDate).format('YYYY-MM-DD');
+      if (startDate > endDate) {
+        this.stamentAccountModel.endDate = '';
+      }
+    }
+    this.minDate = new Date(event);
+
+  }
 }
