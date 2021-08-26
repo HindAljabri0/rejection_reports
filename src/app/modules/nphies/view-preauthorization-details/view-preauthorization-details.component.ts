9530c57145acc7a52d13deed344ad3d8543bc5fb
Akshat Mewada <akshat.mewada@tatvasoft.com>
2021-08-26 17:56:23 +0530

    Modified items in approval details

--- src/app/modules/nphies/view-preauthorization-details/view-preauthorization-details.component.ts
+++ src/app/modules/nphies/view-preauthorization-details/view-preauthorization-details.component.ts
@@ -7,7 +7,7 @@ import { MatDialog, MatDialogRef } from '@angular/material';
   styles: []
 })
 export class ViewPreauthorizationDetailsComponent implements OnInit {
-
+  currentSelectedItem = -1;
   constructor(
     private dialogRef: MatDialogRef<ViewPreauthorizationDetailsComponent>
   ) { }
@@ -17,6 +17,14 @@ export class ViewPreauthorizationDetailsComponent implements OnInit {
 
   closeDialog() {
     this.dialogRef.close();
+  }
+
+  toggleItem(index) {
+    if (this.currentSelectedItem == index) {
+      this.currentSelectedItem = -1;
+    } else {
+      this.currentSelectedItem = index;
+    }
   }
 
 }
