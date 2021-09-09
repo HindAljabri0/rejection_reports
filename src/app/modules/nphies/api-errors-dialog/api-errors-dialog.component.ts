53dfe0c21f2c52b12d35944e839d6b769fd8d331
AhmedAlYousif <ahmadalsiddeeq@gmail.com>
2021-09-09 12:11:22 +0300

    handle API errors for eligibility request

---
+++ src/app/modules/nphies/api-errors-dialog/api-errors-dialog.component.ts
@@ -0,0 +1,24 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import { ApiError } from 'src/app/models/nphies/apiError';
+
+@Component({
+  selector: 'app-api-errors-dialog',
+  templateUrl: './api-errors-dialog.component.html',
+  styleUrls: ['./api-errors-dialog.component.css']
+})
+export class ApiErrorsDialogComponent implements OnInit {
+
+  constructor(
+    private dialogRef: MatDialogRef<ApiErrorsDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: ApiError
+  ) { }
+
+  ngOnInit() {
+  }
+
+  closeDialog() {
+    this.dialogRef.close();
+  }
+  
+}
