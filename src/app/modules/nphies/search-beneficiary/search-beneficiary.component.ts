c6dd26ad6f6e9f1d0c87fee4ffe2eea10ca24aae
omarWaseel <omar.alohideb@waseel.com>
2021-09-19 09:47:40 +0300

    edit search field  based on criteria

--- src/app/modules/nphies/search-beneficiary/search-beneficiary.component.ts
+++ src/app/modules/nphies/search-beneficiary/search-beneficiary.component.ts
@@ -1,5 +1,6 @@
 import { HttpErrorResponse, HttpResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 import { PageEvent } from '@angular/material';
 import { BeneficiarySearch } from 'src/app/models/nphies/beneficiarySearch';
 import { DialogService } from 'src/app/services/dialogsService/dialog.service';
@@ -13,7 +14,7 @@ import { SharedServices } from 'src/app/services/shared.services';
 
 })
 export class SearchBeneficiaryComponent implements OnInit {
-  beneficiaries: BeneficiarySearch[] = [];
+  beneficiaries: BeneficiarySearch[] ;
   constructor(private sharedServices: SharedServices, private providersBeneficiariesService: ProvidersBeneficiariesService, private providerNphiesSearchService: ProviderNphiesSearchService, private dialogService: DialogService) { }
 
   length = 100;
@@ -21,28 +22,22 @@ export class SearchBeneficiaryComponent implements OnInit {
   pageIndex = 0;
   pageSizeOptions = [10, 50, 100];
   showFirstLastButtons = true;
+
+
+  nameController: FormControl = new FormControl();
+  nationalIdController: FormControl = new FormControl();
+  contactNoController: FormControl = new FormControl();
+  fileIdController: FormControl = new FormControl();
+  memberCardidController: FormControl = new FormControl();
+
+
   handlePageEvent(event: PageEvent) {
-    this.beneficiaries = [];
+   // this.beneficiaries = [];
     this.length = event.length;
     this.pageSize = event.pageSize;
     this.pageIndex = event.pageIndex;
     localStorage.setItem('pagesize', event.pageSize + '');
-    console.log(event);
-    this.providerNphiesSearchService.NphisBeneficiarySearchByCriteria(this.sharedServices.providerId, null, null, null, null, null, this.pageIndex, this.pageSize).subscribe(event => {
-      if (event instanceof HttpResponse) {
-        if (event.body != null && event.body instanceof Array)
-          this.beneficiaries = [];
-        this.beneficiaries = event.body["content"] as BeneficiarySearch[];
-      }
-    }
-      , err => {
-
-        if (err instanceof HttpErrorResponse) {
-          console.log(err.message)
-
-
-        }
-      });
+    this.searchByCriteria();
 
   }
 
@@ -64,6 +59,29 @@ export class SearchBeneficiaryComponent implements OnInit {
 
         }
       });
+
+  }
+
+  searchByCriteria() {
+
+      this.providerNphiesSearchService.NphisBeneficiarySearchByCriteria(this.sharedServices.providerId,
+      this.nationalIdController.value, this.nameController.value, this.memberCardidController.value,
+      this.fileIdController.value, this.contactNoController.value, this.pageIndex, this.pageSize).subscribe(event => {
+        if (event instanceof HttpResponse) {
+          if (event.body != null && event.body instanceof Array)
+            this.beneficiaries = [];
+          this.beneficiaries = event.body["content"] as BeneficiarySearch[];
+          this.length = event.body["totalElements"]
+        }
+      }
+        , err => {
+
+          if (err instanceof HttpErrorResponse) {
+            console.log(err.message)
+
+
+          }
+        });
 
   }
 
