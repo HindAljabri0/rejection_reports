3f851556232698a06ecc26485993da371db1a4d2
omarWaseel <omar.alohideb@waseel.com>
2021-11-02 22:22:30 +0300

    edit on gss report page

--- src/app/pages/reports/general-summary-statement-report/general-summary-statement-report.component.ts
+++ src/app/pages/reports/general-summary-statement-report/general-summary-statement-report.component.ts
@@ -5,7 +5,10 @@ import { FormControl, FormGroup } from '@angular/forms';
 import { MatPaginator } from '@angular/material';
 import * as moment from 'moment';
 import { BsDatepickerConfig } from 'ngx-bootstrap';
+import { Subscription } from 'rxjs';
+import { DownloadStatus } from 'src/app/models/downloadRequest';
 import { GssReportResponseModel } from 'src/app/models/gssReportResponseModel';
+import { DownloadService } from 'src/app/services/downloadService/download.service';
 import { SearchService } from 'src/app/services/serchService/search.service';
 import { SharedServices } from 'src/app/services/shared.services';
 
@@ -19,21 +22,21 @@ export class GeneralSummaryStatementReportComponent implements OnInit {
   payers: { id: string, name: string }[]=[];
   paginatorPageSizeOptions = [10, 20, 50, 100];
   paginatorPagesNumbers: number[]=[];
-  page: number;
-  pageSize: number;
-  tempPage = 0;
-  tempPageSize = 10;
+  page: number=0;
+  pageSize: number=10;
+ 
   paginatorLength1=0;
   payerId='';
   fromDate = new FormControl();
   toDate=new FormControl();
   errorMessage: string;
+  lastDownloadSubscriptions: Subscription;
   @ViewChild('paginator', { static: false }) paginator: MatPaginator;
   gssReportResponseModel:GssReportResponseModel[];
   datePickerConfig: Partial<BsDatepickerConfig> = { showWeekNumbers: false, dateInputFormat: 'DD/MM/YYYY' };
   manualPage = 0;
   minDate: any;
-  constructor( public datepipe: DatePipe,public commen: SharedServices,public searchService:SearchService) { }
+  constructor(  private downloadService: DownloadService,public datepipe: DatePipe,public commen: SharedServices,public searchService:SearchService) { }
 
   ngOnInit() {
 
@@ -55,7 +58,7 @@ export class GeneralSummaryStatementReportComponent implements OnInit {
     this.gssReportResponseModel=[];
     let fromDate =this.datepipe.transform(this.fromDate.value, 'dd-MM-yyyy');
     let toDate = this.datepipe.transform(this.toDate.value, 'dd-MM-yyyy');;
-    this.searchService.getGssData(this.commen.providerId,this.payerId,fromDate,toDate,this.tempPage,this.tempPageSize).subscribe((data:any)=>{
+    this.searchService.getGssData(this.commen.providerId,this.payerId,fromDate,toDate, this.page, this.pageSize).subscribe((data:any)=>{
       if (data instanceof HttpResponse) {
         this.gssReportResponseModel=data.body["content"] as GssReportResponseModel[];
         this.paginatorLength1=data.body["totalElements"];
@@ -122,5 +125,28 @@ export class GeneralSummaryStatementReportComponent implements OnInit {
         pageSize: this.paginator.pageSize,
         length: this.paginator.length
       });
+    }
+
+    download() {
+      if (this.detailTopActionIcon == 'ic-check-circle.svg') {
+        return;
+      }
+      let fromDate =this.datepipe.transform(this.fromDate.value, 'dd-MM-yyyy');
+    let toDate = this.datepipe.transform(this.toDate.value, 'dd-MM-yyyy');;
+    
+      this.lastDownloadSubscriptions = this.downloadService
+        .startDownload(this.searchService.downloadGssReport(
+            this.commen.providerId,
+            this.payerId,
+            fromDate,
+            toDate
+          ))
+        .subscribe(status => {
+          if (status != DownloadStatus.ERROR) {
+            this.detailTopActionIcon = 'ic-check-circle.svg';
+          } else {
+            this.detailTopActionIcon = 'ic-download.svg';
+          }
+        });
     }
 }
