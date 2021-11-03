226bea85bb7b43e3a66f21c8ff73c6b3f2945159
AhmedAlYousif <ahmadalsiddeeq@gmail.com>
2021-11-03 17:51:54 +0300

    gss all payers

--- src/app/pages/reports/general-summary-statement-report/general-summary-statement-report.component.ts
+++ src/app/pages/reports/general-summary-statement-report/general-summary-statement-report.component.ts
@@ -1,7 +1,7 @@
 import { I } from '@angular/cdk/keycodes';
 import { DatePipe } from '@angular/common';
 import { HttpErrorResponse, HttpResponse } from '@angular/common/http';
-import {  Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { MatPaginator } from '@angular/material';
 import * as moment from 'moment';
@@ -16,67 +16,68 @@ import { SharedServices } from 'src/app/services/shared.services';
 @Component({
   selector: 'app-general-summary-statement-report',
   templateUrl: './general-summary-statement-report.component.html',
-  styles:[]
+  styles: []
 })
 export class GeneralSummaryStatementReportComponent implements OnInit {
   detailTopActionIcon = 'ic-download.svg';
-  payers: { id: string, name: string }[]=[];
+  payers: { id: string, name: string }[] = [{id: 'All', name: 'All'}];
   paginatorPageSizeOptions = [10, 20, 50, 100];
-  paginatorPagesNumbers: number[]=[];
-  page: number=0;
-  pageSize: number=10;
-  submitted=false;
-  paginatorLength1=0;
-  payerId='';
+  paginatorPagesNumbers: number[] = [];
+  page: number = 0;
+  pageSize: number = 10;
+  submitted = false;
+  paginatorLength1 = 0;
+  payerId = '';
   fromDate = new FormControl();
-  toDate=new FormControl();
+  toDate = new FormControl();
   errorMessage: string;
   lastDownloadSubscriptions: Subscription;
   @ViewChild('paginator', { static: false }) paginator: MatPaginator;
-  gssReportResponseModel:GssReportResponseModel[];
+  gssReportResponseModel: GssReportResponseModel[];
   datePickerConfig: Partial<BsDatepickerConfig> = { showWeekNumbers: false, dateInputFormat: 'DD/MM/YYYY' };
   manualPage = 0;
   minDate: any;
-  constructor(  private downloadService: DownloadService,public datepipe: DatePipe,public commen: SharedServices,public searchService:SearchService) { }
+  constructor(private downloadService: DownloadService, public datepipe: DatePipe, public commen: SharedServices, public searchService: SearchService) { }
 
   ngOnInit() {
 
     //this.payers = [];
-  
+
     this.commen.getPayersList().map(value => {
       this.payers.push({
         id: `${value.id}`,
         name: value.name
       });
-     
+
     });
-   
+
   }
 
   search() {
-    this.submitted=true;
-    if((this.fromDate.value==null || this.fromDate.value=='')||(this.toDate.value==null || this.toDate.value=='')||
-    (this.payerId==null || this.payerId=='')){
-     return 
+    this.submitted = true;
+    if ((this.fromDate.value == null || this.fromDate.value == '') || (this.toDate.value == null || this.toDate.value == '') ||
+      (this.payerId == null || this.payerId == '')) {
+      return
     }
-    this.errorMessage =null;
+    this.errorMessage = null;
     this.commen.loadingChanged.next(true);
-    this.gssReportResponseModel=[];
-    let fromDate =this.datepipe.transform(this.fromDate.value, 'dd-MM-yyyy');
+    this.gssReportResponseModel = [];
+    let fromDate = this.datepipe.transform(this.fromDate.value, 'dd-MM-yyyy');
     let toDate = this.datepipe.transform(this.toDate.value, 'dd-MM-yyyy');;
-    this.searchService.getGssData(this.commen.providerId,this.payerId,fromDate,toDate, this.page, this.pageSize).subscribe((data:any)=>{
+    this.searchService.getGssData(this.commen.providerId, this.payerId, fromDate, toDate, this.page, this.pageSize).subscribe((data: any) => {
       if (data instanceof HttpResponse) {
-        this.gssReportResponseModel=data.body["content"] as GssReportResponseModel[];
-        this.paginatorLength1=data.body["totalElements"];
-        this.manualPage =data.body["number"];
+        this.gssReportResponseModel = data.body["content"] as GssReportResponseModel[];
+        this.paginatorLength1 = data.body["totalElements"];
+        this.manualPage = data.body["number"];
         const pages = Math.ceil((this.paginatorLength1 / this.paginator.pageSize));
-          this.paginatorPagesNumbers = Array(pages).fill(pages).map((x, i) => i);
-          if (this.gssReportResponseModel.length == 0) {
-            this.errorMessage = 'No Results Found';
-          }
-          this.commen.loadingChanged.next(false);
-        
-    }}, error => {
+        this.paginatorPagesNumbers = Array(pages).fill(pages).map((x, i) => i);
+        if (this.gssReportResponseModel.length == 0) {
+          this.errorMessage = 'No Results Found';
+        }
+        this.commen.loadingChanged.next(false);
+
+      }
+    }, error => {
       if (error instanceof HttpErrorResponse) {
         if ((error.status / 100).toFixed() == '4') {
           this.errorMessage = 'Access Denied.';
@@ -85,74 +86,75 @@ export class GeneralSummaryStatementReportComponent implements OnInit {
         } else {
           this.errorMessage = 'Somthing went wrong.';
         }
-     
-          this.commen.loadingChanged.next(false);
-     }});
-    
-    }
 
-    dateValidation(event: any) {
-      if (event !== null) {
-        const startDate = moment(event).format('YYYY-MM-DD');
-        const endDate = moment(this.toDate.value).format('YYYY-MM-DD');
-        if (startDate > endDate) {
-          this.toDate.value.patchValue('');
-        }
+        this.commen.loadingChanged.next(false);
       }
-      this.minDate = new Date(event);
-  
-    }
-    paginationChange(event) {
-      this.page = event.pageIndex;
-      this.pageSize = event.pageSize;
-      // this.resetURL();
-      this.search();
-    }
-    get paginatorLength() {
-      if (this.gssReportResponseModel != null) {
-        return this.paginatorLength1;
-      } else {
-        return 0;
-      }
-    }
-    paginatorAction(event) {
-      this.manualPage = event['pageIndex'];
-      this.paginationChange(event);
-      this.page = event.pageIndex;
-      this.pageSize = event.pageSize;
-      this.search();
-    }
-    updateManualPage(index) {
-      this.manualPage = index;
-      this.paginator.pageIndex = index;
-      this.paginatorAction({
-        previousPageIndex: this.paginator.pageIndex,
-        pageIndex: index,
-        pageSize: this.paginator.pageSize,
-        length: this.paginator.length
-      });
-    }
+    });
 
-    download() {
-      if (this.detailTopActionIcon == 'ic-check-circle.svg') {
-        return;
+  }
+
+  dateValidation(event: any) {
+    if (event !== null) {
+      const startDate = moment(event).format('YYYY-MM-DD');
+      const endDate = moment(this.toDate.value).format('YYYY-MM-DD');
+      if (startDate > endDate) {
+        this.toDate.value.patchValue('');
       }
-      let fromDate =this.datepipe.transform(this.fromDate.value, 'dd-MM-yyyy');
+    }
+    this.minDate = new Date(event);
+
+  }
+  paginationChange(event) {
+    this.page = event.pageIndex;
+    this.pageSize = event.pageSize;
+    // this.resetURL();
+    this.search();
+  }
+  get paginatorLength() {
+    if (this.gssReportResponseModel != null) {
+      return this.paginatorLength1;
+    } else {
+      return 0;
+    }
+  }
+  paginatorAction(event) {
+    this.manualPage = event['pageIndex'];
+    this.paginationChange(event);
+    this.page = event.pageIndex;
+    this.pageSize = event.pageSize;
+    this.search();
+  }
+  updateManualPage(index) {
+    this.manualPage = index;
+    this.paginator.pageIndex = index;
+    this.paginatorAction({
+      previousPageIndex: this.paginator.pageIndex,
+      pageIndex: index,
+      pageSize: this.paginator.pageSize,
+      length: this.paginator.length
+    });
+  }
+
+  download() {
+    if (this.detailTopActionIcon == 'ic-check-circle.svg') {
+      return;
+    }
+    let fromDate = this.datepipe.transform(this.fromDate.value, 'dd-MM-yyyy');
     let toDate = this.datepipe.transform(this.toDate.value, 'dd-MM-yyyy');;
-    
-      this.lastDownloadSubscriptions = this.downloadService
-        .startDownload(this.searchService.downloadGssReport(
-            this.commen.providerId,
-            this.payerId,
-            fromDate,
-            toDate
-          ))
-        .subscribe(status => {
-          if (status != DownloadStatus.ERROR) {
-            this.detailTopActionIcon = 'ic-check-circle.svg';
-          } else {
-            this.detailTopActionIcon = 'ic-download.svg';
-          }
-        });
-    }
+
+    this.lastDownloadSubscriptions = this.downloadService
+      .startDownload(this.searchService.downloadGssReport(
+        this.commen.providerId,
+        this.payerId == 'All' ? this.payers.map(payer => payer.id) : [this.payerId],
+        fromDate,
+        toDate
+      ))
+      .subscribe(status => {
+        if (status != DownloadStatus.ERROR) {
+          this.detailTopActionIcon = 'ic-check-circle.svg';
+        } else {
+          this.detailTopActionIcon = 'ic-download.svg';
+        }
+      });
+  }
 }
--- src/app/services/serchService/search.service.ts
+++ src/app/services/serchService/search.service.ts
@@ -317,8 +317,8 @@ export class SearchService {
     const request = new HttpRequest('GET', environment.claimSearchHost + requestUrl);
     return this.http.request(request);
   }
-  downloadGssReport(providerId: string, payer: string, fromDate: string, toDate: string) {
-    const requestUrl = `/providers/${providerId}/gss/download/pdf?payer=${payer}&fromDate=${fromDate}&toDate=${toDate}`;
+  downloadGssReport(providerId: string, payer: string[], fromDate: string, toDate: string) {
+    const requestUrl = `/providers/${providerId}/gss/download/pdf?payer=${payer.join(',')}&fromDate=${fromDate}&toDate=${toDate}`;
     const headers: HttpHeaders = new HttpHeaders('Content-Type: application/pdf');
     const request = new HttpRequest('GET', environment.claimSearchHost + requestUrl,'', 
       { responseType: 'blob', reportProgress: true, headers: headers });
