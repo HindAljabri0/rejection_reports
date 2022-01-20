5eb27b86611252c88d9ee889137cdb62a7b04878
omarWaseel <omar.alohideb@waseel.com>
2022-01-20 12:59:00 +0300

    add message error when invoice has duplicate service

--- src/app/claim-module-components/invoices-services/invoices-services.component.ts
+++ src/app/claim-module-components/invoices-services/invoices-services.component.ts
@@ -220,6 +220,14 @@ export class InvoicesServicesComponent implements OnInit, OnDestroy {
     this._onDestroy.next();
     this._onDestroy.complete();
   }
+
+  
+ invoiceHasDuplicateService(invoice: Invoice){
+  let invoicesHasDuplicateService = this.errors.filter(x => Number(x.code) == invoice.invoiceId &&x.error.includes('We found duplicate services in Invoice') );
+  return invoicesHasDuplicateService.length>0?true:false
+}
+
+
   invoiceHasError(index: Invoice) {
     let check =0;
         let invoicesError = this.errors.filter(x => Number(x.code) == index.invoiceId);
