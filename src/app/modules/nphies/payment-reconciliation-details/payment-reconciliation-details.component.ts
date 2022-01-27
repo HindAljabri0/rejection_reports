8d6e1109d7e820abb3601f8abf26914b5100ae8a
Akshat Mewada <akshat.mewada@tatvasoft.com>
2022-01-27 10:58:06 +0530

    Added small message in payment reconciliation payment date

--- src/app/modules/nphies/payment-reconciliation-details/payment-reconciliation-details.component.ts
+++ src/app/modules/nphies/payment-reconciliation-details/payment-reconciliation-details.component.ts
@@ -9,107 +9,107 @@ import { CreateClaimNphiesComponent } from '../create-claim-nphies/create-claim-
 import { MatDialog, MatDialogRef } from '@angular/material';
 
 @Component({
-  selector: 'app-payment-reconciliation-details',
-  templateUrl: './payment-reconciliation-details.component.html',
-  styles: []
+    selector: 'app-payment-reconciliation-details',
+    templateUrl: './payment-reconciliation-details.component.html',
+    styles: []
 })
 export class PaymentReconciliationDetailsComponent implements OnInit {
 
-  reconciliationId: number;
-  reconciliationDetails: any;
+    reconciliationId: number;
+    reconciliationDetails: any;
 
-  currentOpenRecord = -1;
-  params: SearchPageQueryParams = new SearchPageQueryParams();
+    currentOpenRecord = -1;
+    params: SearchPageQueryParams = new SearchPageQueryParams();
 
-  claimId: string;
-  uploadId: string;
-  claimResponseId: string;
+    claimId: string;
+    uploadId: string;
+    claimResponseId: string;
 
-  claimDialogRef: MatDialogRef<any, any>;
+    claimDialogRef: MatDialogRef<any, any>;
 
-  constructor(
-    public dialog: MatDialog,
-    private location: Location,
-    public router: Router,
-    public routeActive: ActivatedRoute,
-    private activatedRoute: ActivatedRoute,
-    private sharedServices: SharedServices,
-    private providerNphiesSearchService: ProviderNphiesSearchService) { }
+    constructor(
+        public dialog: MatDialog,
+        private location: Location,
+        public router: Router,
+        public routeActive: ActivatedRoute,
+        private activatedRoute: ActivatedRoute,
+        private sharedServices: SharedServices,
+        private providerNphiesSearchService: ProviderNphiesSearchService) { }
 
-  ngOnInit() {
-    if (this.activatedRoute.snapshot.params.reconciliationId) {
-      // tslint:disable-next-line:radix
-      this.reconciliationId = parseInt(this.activatedRoute.snapshot.params.reconciliationId);
+    ngOnInit() {
+        if (this.activatedRoute.snapshot.params.reconciliationId) {
+            // tslint:disable-next-line:radix
+            this.reconciliationId = parseInt(this.activatedRoute.snapshot.params.reconciliationId);
+        }
+
+        if (this.reconciliationId) {
+            this.getPaymentReconciliationDetails();
+        }
+
+        if (this.activatedRoute.snapshot.queryParams.claimId) {
+            this.claimId = this.activatedRoute.snapshot.queryParams.claimId;
+        }
+
+        if (this.activatedRoute.snapshot.queryParams.uploadId) {
+            this.uploadId = this.activatedRoute.snapshot.queryParams.uploadId;
+        }
+
+        if (this.activatedRoute.snapshot.queryParams.claimResponseId) {
+            this.claimResponseId = this.activatedRoute.snapshot.queryParams.claimResponseId;
+        }
+
+        if (this.claimId && this.uploadId) {
+            this.showClaim(this.claimId, this.uploadId, this.claimResponseId);
+        }
+
     }
 
-    if (this.reconciliationId) {
-      this.getPaymentReconciliationDetails();
+    getPaymentReconciliationDetails() {
+        this.sharedServices.loadingChanged.next(true);
+        // tslint:disable-next-line:max-line-length
+        this.providerNphiesSearchService.getPaymentReconciliationDetails(this.sharedServices.providerId, this.reconciliationId).subscribe((event: any) => {
+            if (event instanceof HttpResponse) {
+                const body = event.body;
+                this.reconciliationDetails = body;
+                this.sharedServices.loadingChanged.next(false);
+            }
+        }, err => {
+            this.sharedServices.loadingChanged.next(false);
+            console.log(err);
+        });
     }
 
-    if (this.activatedRoute.snapshot.queryParams.claimId) {
-      this.claimId = this.activatedRoute.snapshot.queryParams.claimId;
+    goBack() {
+        this.location.back();
     }
 
-    if (this.activatedRoute.snapshot.queryParams.uploadId) {
-      this.uploadId = this.activatedRoute.snapshot.queryParams.uploadId;
+    toggleRow(index) {
+        this.currentOpenRecord = (index === this.currentOpenRecord) ? -1 : index;
     }
 
-    if (this.activatedRoute.snapshot.queryParams.claimResponseId) {
-      this.claimResponseId = this.activatedRoute.snapshot.queryParams.claimResponseId;
+    showClaim(claimId: string, uploadId: string, claimResponseId: string) {
+        this.params.claimId = claimId;
+        this.params.uploadId = uploadId;
+        this.params.claimResponseId = claimResponseId;
+        this.resetURL();
+        this.claimDialogRef = this.dialog.open(CreateClaimNphiesComponent, {
+            panelClass: ['primary-dialog', 'full-screen-dialog'],
+            autoFocus: false, data: { claimId }
+        });
+
+        this.claimDialogRef.afterClosed().subscribe(result => {
+            this.claimDialogRef = null;
+            this.params.claimId = null;
+            this.params.editMode = null;
+            this.resetURL();
+        });
     }
 
-    if (this.claimId && this.uploadId) {
-      this.showClaim(this.claimId, this.uploadId, this.claimResponseId);
+    resetURL() {
+        this.router.navigate([], {
+            relativeTo: this.routeActive,
+            queryParams: { ...this.params, editMode: null, size: null }
+        });
     }
-
-  }
-
-  getPaymentReconciliationDetails() {
-    this.sharedServices.loadingChanged.next(true);
-    // tslint:disable-next-line:max-line-length
-    this.providerNphiesSearchService.getPaymentReconciliationDetails(this.sharedServices.providerId, this.reconciliationId).subscribe((event: any) => {
-      if (event instanceof HttpResponse) {
-        const body = event.body;
-        this.reconciliationDetails = body;
-        this.sharedServices.loadingChanged.next(false);
-      }
-    }, err => {
-      this.sharedServices.loadingChanged.next(false);
-      console.log(err);
-    });
-  }
-
-  goBack() {
-    this.location.back();
-  }
-
-  toggleRow(index) {
-    this.currentOpenRecord = (index === this.currentOpenRecord) ? -1 : index;
-  }
-
-  showClaim(claimId: string, uploadId: string, claimResponseId: string) {
-    this.params.claimId = claimId;
-    this.params.uploadId = uploadId;
-    this.params.claimResponseId = claimResponseId;
-    this.resetURL();
-    this.claimDialogRef = this.dialog.open(CreateClaimNphiesComponent, {
-      panelClass: ['primary-dialog', 'full-screen-dialog'],
-      autoFocus: false, data: { claimId }
-    });
-
-    this.claimDialogRef.afterClosed().subscribe(result => {
-      this.claimDialogRef = null;
-      this.params.claimId = null;
-      this.params.editMode = null;
-      this.resetURL();
-    });
-  }
-
-  resetURL() {
-    this.router.navigate([], {
-      relativeTo: this.routeActive,
-      queryParams: { ...this.params, editMode: null, size: null }
-    });
-  }
 
 }
