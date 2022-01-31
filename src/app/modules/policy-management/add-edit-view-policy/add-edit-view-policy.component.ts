06a37db348600028e5c39954a3e4d1a56146a219
mohammed.mahjoob <mohammed.mahjoob@tirhal.net>
2022-01-31 16:26:53 +0300

    policy + classes ready

--- src/app/modules/policy-management/add-edit-view-policy/add-edit-view-policy.component.ts
+++ src/app/modules/policy-management/add-edit-view-policy/add-edit-view-policy.component.ts
@@ -17,241 +17,240 @@ import { SharedServices } from 'src/app/services/shared.services';
 import { AddEditViewClassComponent } from '../add-edit-view-class/add-edit-view-class.component';
 
 @Component({
-  selector: 'app-add-edit-view-policy',
-  templateUrl: './add-edit-view-policy.component.html',
-  styles: []
+    selector: 'app-add-edit-view-policy',
+    templateUrl: './add-edit-view-policy.component.html',
+    styles: []
 })
 export class AddEditViewPolicyComponent implements OnInit {
-  contracts:Contract[];
-  policy:Policy;
-  classes:classResponseTemplate[];
-  search:classRequest;
+    contracts: Contract[];
+    policy: Policy;
+    classes: classResponseTemplate[];
+    search: classRequest;
 
-  length = 100;
-  pageSize = 10;
-  pageIndex = 0;
-  pageSizeOptions = [10, 50, 100];
-  showFirstLastButtons = true;
+    length = 100;
+    pageSize = 10;
+    pageIndex = 0;
+    pageSizeOptions = [10, 50, 100];
+    showFirstLastButtons = true;
 
 
-  d = new Date();
-  oneyear = this.d.setDate(this.d.getDate() + 364);
-  ContractController:FormControl = new FormControl();
-  PolicyNoController:FormControl = new FormControl();
-  PolicyNameController:FormControl = new FormControl();
-  StartDateController: FormControl = new FormControl(new Date());
-  EndDateController: FormControl = new FormControl(this.d);
-  IsActiveController: FormControl = new FormControl(true);
-  PolicyIdController:FormControl = new FormControl();
+    d = new Date();
+    oneyear = this.d.setDate(this.d.getDate() + 364);
+    ContractController: FormControl = new FormControl();
+    PolicyNoController: FormControl = new FormControl();
+    PolicyNameController: FormControl = new FormControl();
+    StartDateController: FormControl = new FormControl(new Date());
+    EndDateController: FormControl = new FormControl(this.d);
+    IsActiveController: FormControl = new FormControl(true);
+    PolicyIdController: FormControl = new FormControl();
 
-  errors = {
-    contractId: "",
-    PolicyNo: "",
-    PolicyName: "",
-    IsActive: "",
-    StartDate: "",
-    EndDate: ""
-    
-  }
-  addMode: boolean = false;
-  EditMode: boolean = false;
-  param:PolicySearchModel=new PolicySearchModel();
-  messageError = "";
+    errors = {
+        contractId: "",
+        PolicyNo: "",
+        PolicyName: "",
+        IsActive: "",
+        StartDate: "",
+        EndDate: ""
 
-  constructor(
-    private router: Router,
-    private activatedRoute: ActivatedRoute,
-    private dialog: MatDialog,
-    private sharedServices :SharedServices,
-    private policyService:PolicyService,
-    private dialogService:DialogService,
-    private contractService: ContractService,
-    private classService:ContractClassService
-  ) { }
-
-  ngOnInit() {
-    this.getContractList();
-    this.param.policyId = this.activatedRoute.snapshot.paramMap.get("policyId");
-    this.param.withpagenation = false;
-    
-    var url = this.router.url;
-    //console.log("Contract Id= "+this.param.contractId);
-
-    if (url.endsWith('add') || this.param.policyId == null) {
-      this.addMode = true;
     }
-    else {
-      this.getPolicy(this.param)
-      this.EditMode = true;
-      this.LoadPolicyClasses(this.param.policyId);
-    }
-  }
-  fillSearchData(PolicyId) {
-    this.search = new classRequest();
-    this.search.policyId = PolicyId;
-    this.search.providerId = this.sharedServices.providerId;
-    this.search.page = this.pageIndex;
-    this.search.size = this.pageSize;
-    this.search.Withpagenation = true;
-    
-  }
-  LoadPolicyClasses(PolicyId) {
-    this.fillSearchData(PolicyId);
-    this.classService.getClassBySearchParam(this.search).subscribe(event => {
-      if (event instanceof HttpResponse) {
-        console.log(event.body);
-        if (event.body != null && event.body instanceof Array)
-          this.classes = event.body;
-        //this.length = event.body["totalElements"]
-      }
-    }
-      , err => {
+    addMode: boolean = false;
+    EditMode: boolean = false;
+    param: PolicySearchModel = new PolicySearchModel();
+    messageError = "";
 
-        if (err instanceof HttpErrorResponse) {
-          console.log(err.message)
-          this.classes = null;
+    constructor(
+        private router: Router,
+        private activatedRoute: ActivatedRoute,
+        private dialog: MatDialog,
+        private sharedServices: SharedServices,
+        private policyService: PolicyService,
+        private dialogService: DialogService,
+        private contractService: ContractService,
+        private classService: ContractClassService
+    ) { }
+
+    ngOnInit() {
+        this.getContractList();
+        this.param.policyId = this.activatedRoute.snapshot.paramMap.get("policyId");
+        this.param.withpagenation = false;
+
+        var url = this.router.url;
+        //console.log("Contract Id= "+this.param.contractId);
+
+        if (url.endsWith('add') || this.param.policyId == null) {
+            this.addMode = true;
         }
-      });
-  }
-  getPolicy(param) {
-    this.policyService.getPolicyBySearchParam(param).subscribe(event => {
-      if (event instanceof HttpResponse) {
-        const body = event.body;
-        
-        if (body instanceof Array) {
-          this.fillEditData(body[0]);
+        else {
+            this.getPolicy(this.param)
+            this.EditMode = true;
+            this.LoadPolicyClasses(this.param.policyId);
         }
-      }
-    }, errorEvent => {
-      if (errorEvent instanceof HttpErrorResponse) {
+    }
+    fillSearchData(PolicyId) {
+        this.search = new classRequest();
+        this.search.policyId = PolicyId;
+        this.search.providerId = this.sharedServices.providerId;
+        this.search.page = this.pageIndex;
+        this.search.size = this.pageSize;
+        this.search.Withpagenation = true;
 
-      }
-    });
-  }
-  handlePageEvent(event: PageEvent) {
-
-    this.length = event.length;
-    this.pageSize = event.pageSize;
-    this.pageIndex = event.pageIndex;
-    localStorage.setItem('pagesize', event.pageSize + '');
-    this.LoadPolicyClasses(this.PolicyIdController.value);
-
-  }
-  fillEditData(result) {
-    this.ContractController.setValue(result.contractId);
-    this.PolicyIdController.setValue(result.policyId);
-    this.PolicyNoController.setValue(result.policyNo);
-    this.PolicyNameController.setValue(result.policyName);
-    this.StartDateController.setValue(result.startDate);
-    this.EndDateController.setValue(result.endDate);
-    this.IsActiveController.setValue(result.isActive == 'Y' ? true : false);
-  }
-  openAddClassDialog() {
-    const dialogRef = this.dialog.open(AddEditViewClassComponent,
-      {
-        panelClass: ['primary-dialog', 'dialog-xl']
-      })
-  }
-  checkError() {
-    let thereIsError = false;
-    this.errors.contractId = "";
-    this.errors.PolicyNo = "";
-    this.errors.PolicyName = "";
-    this.errors.StartDate = "";
-    this.errors.EndDate = "";
-   
-    if (this.ContractController == null || this.ContractController.value == null) {
-      this.errors.contractId = "Contract must be specified"
-      thereIsError = true;
     }
-    if (this.PolicyNoController == null || this.PolicyNoController.value == null) {
-      this.errors.PolicyNo = "Policy No must be specified"
-      thereIsError = true;
-    }
-    if (this.PolicyNameController == null || this.PolicyNameController.value == null) {
-      this.errors.PolicyName = "Contract Name must be specified"
-      thereIsError = true;
-    }
-
-    if (this.StartDateController == null || this.StartDateController.value == null) {
-      this.errors.StartDate = "Start Date must be specified"
-      thereIsError = true;
-    }
-    if (this.EndDateController == null || this.EndDateController.value == null) {
-      this.errors.EndDate = "End Date must be specified"
-      thereIsError = true;
-    }
-    if (this.EndDateController != null && this.StartDateController != null && this.StartDateController.value > this.EndDateController.value) {
-      this.errors.EndDate = "End Date must be greater than start date"
-      thereIsError = true;
-    }
-    
-    return thereIsError;
-  }
-  SaveData() {
-    if (this.checkError()) { return }
-    this.policy = new Policy();
-    this.policy.policyId = this.addMode ? 0 : this.PolicyIdController.value;
-    this.policy.contractId =  this.ContractController.value;
-    this.policy.policyNo = this.PolicyNoController.value;
-    this.policy.policyName = this.PolicyNameController.value;
-    this.policy.startDate = this.StartDateController.value;
-    this.policy.endDate = this.EndDateController.value;
-    this.policy.isActive = this.IsActiveController.value ? "Y" : "N";
-    
-    this.policyService.ManipulatePolicy(this.policy).subscribe(event => {
-      if (event instanceof HttpResponse) {
-        console.log(event.body);
-        if (event.body != null) {
-          this.dialogService.openMessageDialog({
-            title: '',
-            message: `Policy Saved successfully`,
-            isError: false
-          }).subscribe(event => { window.location.reload(); });
-          this.sharedServices.loadingChanged.next(false);
+    LoadPolicyClasses(PolicyId) {
+        this.fillSearchData(PolicyId);
+        this.classService.getClassBySearchParam(this.search).subscribe(event => {
+            if (event instanceof HttpResponse) {
+                console.log(event.body);
+                if (event.body != null && event.body instanceof Array)
+                    this.classes = event.body;
+                //this.length = event.body["totalElements"]
+            }
         }
-      }
+            , err => {
+
+                if (err instanceof HttpErrorResponse) {
+                    console.log(err.message)
+                    this.classes = null;
+                }
+            });
     }
-      , err => {
+    getPolicy(param) {
+        this.policyService.getPolicyBySearchParam(param).subscribe(event => {
+            if (event instanceof HttpResponse) {
+                const body = event.body;
 
-        if (err instanceof HttpErrorResponse) {
-          if (err.status == 500) {
-            this.messageError = "could not reach server Please try again later "
+                if (body instanceof Array) {
+                    this.fillEditData(body[0]);
+                }
+            }
+        }, errorEvent => {
+            if (errorEvent instanceof HttpErrorResponse) {
 
-          } else {
-            this.messageError = err.message;
-          }
-
-          this.dialogService.openMessageDialog({
-            title: '',
-
-            message: this.messageError,
-            isError: true
-          });
-          this.sharedServices.loadingChanged.next(false);
-        }
-      });
-  }
-  getContractList() {
-    let _search = new ContractSearchModel();
-    _search.providerId = this.sharedServices.providerId;
-    _search.page = 0;
-    _search.size = 100000;
-    this.contractService.getContractBySearchParam(_search).subscribe(event => {
-      if (event instanceof HttpResponse) {
-        console.log(event.body);
-        if (event.body != null && event.body instanceof Array)
-          this.contracts = event.body;
-        //this.length = event.body["totalElements"]
-      }
+            }
+        });
     }
-      , err => {
+    handlePageEvent(event: PageEvent) {
 
-        if (err instanceof HttpErrorResponse) {
-          console.log(err.message)
-          this.contracts = null;
+        this.length = event.length;
+        this.pageSize = event.pageSize;
+        this.pageIndex = event.pageIndex;
+        localStorage.setItem('pagesize', event.pageSize + '');
+        this.LoadPolicyClasses(this.PolicyIdController.value);
+
+    }
+    fillEditData(result) {
+        this.ContractController.setValue(result.contractId);
+        this.PolicyIdController.setValue(result.policyId);
+        this.PolicyNoController.setValue(result.policyNo);
+        this.PolicyNameController.setValue(result.policyName);
+        this.StartDateController.setValue(result.startDate);
+        this.EndDateController.setValue(result.endDate);
+        this.IsActiveController.setValue(result.isActive == 'Y' ? true : false);
+    }
+    openAddClassDialog() {
+        const dialogRef = this.dialog.open(AddEditViewClassComponent,
+            {
+                panelClass: ['primary-dialog', 'dialog-xl']
+            })
+    }
+    checkError() {
+        let thereIsError = false;
+        this.errors.contractId = "";
+        this.errors.PolicyNo = "";
+        this.errors.PolicyName = "";
+        this.errors.StartDate = "";
+        this.errors.EndDate = "";
+
+        if (this.ContractController == null || this.ContractController.value == null) {
+            this.errors.contractId = "Contract must be specified"
+            thereIsError = true;
         }
-      });
-  }
+        if (this.PolicyNoController == null || this.PolicyNoController.value == null) {
+            this.errors.PolicyNo = "Policy No must be specified"
+            thereIsError = true;
+        }
+        if (this.PolicyNameController == null || this.PolicyNameController.value == null) {
+            this.errors.PolicyName = "Contract Name must be specified"
+            thereIsError = true;
+        }
+
+        if (this.StartDateController == null || this.StartDateController.value == null) {
+            this.errors.StartDate = "Start Date must be specified"
+            thereIsError = true;
+        }
+        if (this.EndDateController == null || this.EndDateController.value == null) {
+            this.errors.EndDate = "End Date must be specified"
+            thereIsError = true;
+        }
+        if (this.EndDateController != null && this.StartDateController != null && this.StartDateController.value > this.EndDateController.value) {
+            this.errors.EndDate = "End Date must be greater than start date"
+            thereIsError = true;
+        }
+
+        return thereIsError;
+    }
+    SaveData() {
+        if (this.checkError()) { return }
+        this.policy = new Policy();
+        this.policy.policyId = this.addMode ? 0 : this.PolicyIdController.value;
+        this.policy.contractId = this.ContractController.value;
+        this.policy.policyNo = this.PolicyNoController.value;
+        this.policy.policyName = this.PolicyNameController.value;
+        this.policy.startDate = this.StartDateController.value;
+        this.policy.endDate = this.EndDateController.value;
+        this.policy.isActive = this.IsActiveController.value ? "Y" : "N";
+
+        this.policyService.ManipulatePolicy(this.policy).subscribe(event => {
+            if (event instanceof HttpResponse) {
+                console.log(event.body);
+                if (event.body != null) {
+                    this.dialogService.openMessageDialog({
+                        title: '',
+                        message: `Policy Saved successfully`,
+                        isError: false
+                    }).subscribe(event => { window.location.reload(); });
+                    this.sharedServices.loadingChanged.next(false);
+                }
+            }
+        }
+            , err => {
+
+                if (err instanceof HttpErrorResponse) {
+                    if (err.status == 500) {
+                        this.messageError = "could not reach server Please try again later "
+
+                    } else {
+                        this.messageError = err.message;
+                    }
+
+                    this.dialogService.openMessageDialog({
+                        title: '',
+
+                        message: this.messageError,
+                        isError: true
+                    });
+                    this.sharedServices.loadingChanged.next(false);
+                }
+            });
+    }
+    getContractList() {
+        let _search = new ContractSearchModel();
+        _search.providerId = this.sharedServices.providerId;
+        _search.withPagination = false;
+        this.contractService.getContractBySearchParam(_search).subscribe(event => {
+            if (event instanceof HttpResponse) {
+                console.log(event.body);
+                if (event.body != null && event.body instanceof Array)
+                    this.contracts = event.body;
+                //this.length = event.body["totalElements"]
+            }
+        }
+            , err => {
+
+                if (err instanceof HttpErrorResponse) {
+                    console.log(err.message)
+                    this.contracts = null;
+                }
+            });
+    }
 
 }
