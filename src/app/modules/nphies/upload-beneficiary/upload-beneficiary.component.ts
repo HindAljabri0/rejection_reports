527fa37f2b7a09c1a8d9b7a463befd39499b37e3
Akshat Mewada <akshat.mewada@tatvasoft.com>
2022-03-28 15:09:19 +0530

    Added page to upload beneficiaries

---
+++ src/app/modules/nphies/upload-beneficiary/upload-beneficiary.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-upload-beneficiary',
+  templateUrl: './upload-beneficiary.component.html',
+  styles: []
+})
+export class UploadBeneficiaryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
