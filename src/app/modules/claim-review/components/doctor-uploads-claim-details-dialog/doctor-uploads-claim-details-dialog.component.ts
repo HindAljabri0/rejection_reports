2d1c63f11f57a2ea992205fa1046b68e569108c3
Akshat Mewada <akshat.mewada@tatvasoft.com>
2022-04-27 15:19:59 +0530

    Added design for cliam scrubbing doctor

---
+++ src/app/modules/claim-review/components/doctor-uploads-claim-details-dialog/doctor-uploads-claim-details-dialog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-doctor-uploads-claim-details-dialog',
+  templateUrl: './doctor-uploads-claim-details-dialog.component.html',
+  styles: []
+})
+export class DoctorUploadsClaimDetailsDialogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
