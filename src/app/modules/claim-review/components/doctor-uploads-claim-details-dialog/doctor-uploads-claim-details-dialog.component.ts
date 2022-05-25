06772b9cff136ab6cd9c6c5005372d7e517e7ffc
sajad27 <sajad-2020@hotmail.com>
2022-05-25 15:47:13 +0300

    bug fix, stop paginating for first and last claim

--- src/app/modules/claim-review/components/doctor-uploads-claim-details-dialog/doctor-uploads-claim-details-dialog.component.ts
+++ src/app/modules/claim-review/components/doctor-uploads-claim-details-dialog/doctor-uploads-claim-details-dialog.component.ts
@@ -43,7 +43,6 @@ export class DoctorUploadsClaimDetailsDialogComponent implements OnInit {
   
   ngOnInit() {
     this.initVariables();
-    console.log('dialog data: ', this.data);
   }
 
   initVariables() {
