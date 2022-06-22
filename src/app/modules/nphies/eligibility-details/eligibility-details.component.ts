6b085e76815ab21a8a24a50a02d8ff7d4833ad26
omarWaseel <omar.alohideb@waseel.com>
2022-06-22 11:47:33 +0300

    display insure in eligiblity response page

--- src/app/modules/nphies/eligibility-details/eligibility-details.component.ts
+++ src/app/modules/nphies/eligibility-details/eligibility-details.component.ts
@@ -1,5 +1,8 @@
+import { HttpErrorResponse, HttpResponse } from '@angular/common/http';
 import { Component, Input, OnInit } from '@angular/core';
 import { EligibilityResponseModel } from 'src/app/models/nphies/eligibilityResponseModel';
+import { Payer } from 'src/app/models/nphies/payer';
+import { ProviderNphiesSearchService } from 'src/app/services/providerNphiesSearchService/provider-nphies-search.service';
 import { SharedDataService } from 'src/app/services/sharedDataService/shared-data.service';
 
 @Component({
@@ -11,12 +14,13 @@ export class EligibilityDetailsComponent implements OnInit {
 
   currentOpenCoverage: number = null;
   @Input() eligibilityResponse: EligibilityResponseModel;
-
-  constructor(private sharedDataService: SharedDataService) { }
+  payers:Payer[]=[];
+  constructor(private sharedDataService: SharedDataService,private nphiesSearchService: ProviderNphiesSearchService) { }
 
   eligibiltyTypeList = this.sharedDataService.beneficiaryTypeList;
 
   ngOnInit() {
+    this.getPayers();
     // tslint:disable-next-line:max-line-length
     this.eligibilityResponse.siteEligibilityName = this.sharedDataService.siteEligibility.filter(x => x.value === this.eligibilityResponse.siteEligibility)[0] ? this.sharedDataService.siteEligibility.filter(x => x.value === this.eligibilityResponse.siteEligibility)[0].value + ' ( ' + this.sharedDataService.siteEligibility.filter(x => x.value === this.eligibilityResponse.siteEligibility)[0].name + ' )' : '-';
     this.eligibilityResponse.documentTypeName = this.eligibiltyTypeList.filter(x => x.value === this.eligibilityResponse.documentType)[0] ? this.eligibiltyTypeList.filter(x => x.value === this.eligibilityResponse.documentType)[0].name : '-';
@@ -25,6 +29,28 @@ export class EligibilityDetailsComponent implements OnInit {
 
   toggleRow(index) {
     this.currentOpenCoverage = (index == this.currentOpenCoverage) ? -1 : index;
+  }
+
+  getNamePayer( payerNphiesId:String){
+    console.log(payerNphiesId)
+    var payer=  this.payers.find(val => val.nphiesId == payerNphiesId);
+    console.log(payer.englistName)
+    return payer.englistName;
+  }
+  getPayers() {
+    this.nphiesSearchService.getPayersNotTBA().subscribe(event => {
+      if (event instanceof HttpResponse) {
+       
+      
+          this.payers = event.body as Payer[] ;
+          
+       
+      }
+    }, errorEvent => {
+      if (errorEvent instanceof HttpErrorResponse) {
+       
+      }
+    });
   }
 
 }
