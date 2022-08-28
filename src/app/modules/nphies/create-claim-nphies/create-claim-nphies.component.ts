1f7e09075427b51cfb826dd5f7bef24174f25cca
mohammed.mahjoob <mohammed.mahjoob@tirhal.net>
2022-08-28 11:23:54 +0300

    fix delete error msg and add edit to rejected

--- src/app/modules/nphies/create-claim-nphies/create-claim-nphies.component.ts
+++ src/app/modules/nphies/create-claim-nphies/create-claim-nphies.component.ts
@@ -3051,7 +3051,7 @@ export class CreateClaimNphiesComponent implements OnInit {
   get claimIsEditable() {
     return this.otherDataModel != null
       && this.otherDataModel.status != null
-      && ['accepted', 'cancelled', 'notaccepted', 'error', 'invalid', 'failed'].includes(this.otherDataModel.status.trim().toLowerCase());
+      && ['accepted', 'cancelled', 'notaccepted', 'error', 'invalid','rejected', 'failed'].includes(this.otherDataModel.status.trim().toLowerCase());
   }
 
   get IsPreAuthRefRequired() {
