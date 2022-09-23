481441eebfa76529daec5d47ec00ab9f2e881ade
Akshat Mewada <akshat.mewada@tatvasoft.com>
2022-09-23 17:11:13 +0530

    Added nphies attachment configuration for admin

---
+++ src/app/pages/nphies-attachment-configuration/nphies-attachment-configuration.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nphies-attachment-configuration',
+  templateUrl: './nphies-attachment-configuration.component.html',
+  styles: []
+})
+export class NphiesAttachmentConfigurationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
