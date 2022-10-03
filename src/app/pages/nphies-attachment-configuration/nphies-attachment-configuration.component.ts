8a75ca3c86dd42678d5e68961896d331eef178b8
Tejas Bhatt <tejas.bhatt@tatvasoft.com>
2022-10-03 15:43:35 +0530

    6000 : Changes related to validations of image for size and format and implemented image compression

--- src/app/pages/nphies-attachment-configuration/nphies-attachment-configuration.component.ts
+++ src/app/pages/nphies-attachment-configuration/nphies-attachment-configuration.component.ts
@@ -6,7 +6,16 @@ import { Router } from '@angular/router';
 import { SharedServices } from 'src/app/services/shared.services';
 import { SettingsService } from 'src/app/services/settingsService/settings.service';
 import { DialogService } from 'src/app/services/dialogsService/dialog.service';
+import { take } from 'rxjs/operators';
+import { Observable } from 'rxjs'
 
+
+// in bytes, compress images larger than 1MB
+const fileSizeMax = 1 * 1024 * 1024
+// in pixels, compress images have the width or height larger than 1024px
+const widthHeightMax = 1024
+const defaultWidthHeightRatio = 1
+const defaultQualityRatio = 0.7
 
 @Component({
   selector: 'app-nphies-attachment-configuration',
@@ -123,6 +132,21 @@ export class NphiesAttachmentConfigurationComponent implements OnInit {
           this.headerFileName = result.headerFileName;
           this.enabled = result.isEnabled
           this.selectedProvider = result.providerId;
+
+          this.isHeaderSize = false;
+          this.isHeaderNotSubmitted = false;
+          this.isHeaderFormat = false;
+          if (this.isSubmitted) {
+            if (!this.headerFile) this.isHeaderNotSubmitted = true;
+          }
+          
+          this.isFooterSize = false;
+          this.isFooterNotSubmitted = false;
+          this.isFooterFormat = false;
+          if (this.isSubmitted) {
+            if (!this.footerFile) this.isFooterNotSubmitted = true;
+          }
+          
           this.sharedServices.loadingChanged.next(false);
         }
         else {
@@ -159,88 +183,132 @@ export class NphiesAttachmentConfigurationComponent implements OnInit {
 
   selectHeaderFile(event) {
     this.HeadersizeInMB = '';
-    this.HeaderFormat = event.target.files[0].name.split('.').pop();
-    this.headerFileName = event.target.files[0].name;
-    this.HeadersizeInMB = this.sharedServices.formatBytes(event.target.files[0].size);
-    if (!this.ImageFormat.includes(this.HeaderFormat.toLowerCase())) {
+
+    if (!this.ImageFormat.includes(event.target.files[0].name.split('.').pop().toLowerCase())) {
       this.isHeaderFormat = true;
       this.isHeaderSize = false;
-      this.isHeaderDimension = false;
+      // this.isHeaderDimension = false;
       this.isHeaderNotSubmitted = false;
     }
-    const reader = new FileReader();
-    reader.readAsDataURL(event.target.files[0]);
-    reader.onloadend = () => {
-      const data: string = reader.result as string;
-      this.header = data.substring(data.indexOf(',') + 1);
-      const img = new Image();
-      img.src = reader.result as string;
-      img.onload = () => {
-        this.headerHeight = img.naturalHeight;
-        this.headerWidth = img.naturalWidth;
-        if (!this.HeadersizeInMB.includes('KB') ||
-          !(this.headerHeight <= 100 && this.headerWidth <= 500)) {
-          this.isHeaderNotSubmitted = false;
-          if (this.HeadersizeInMB.includes('KB')) {
-            var hdSize = parseFloat(this.HeadersizeInMB.replace("KB", "").trim());
-            if (hdSize > 300) {
-              this.isHeaderSize = true;
-            }
-          }
-          if (!(this.headerHeight <= 100 && this.headerWidth <= 500)) this.isHeaderDimension = true;
-        } else {
-          this.headerFile = event.target.files[0];
-          this.isHeaderSize = false;
-          this.isHeaderDimension = false;
-          this.isHeaderNotSubmitted = false;
-          this.isHeaderFormat = false;
-        }
-      };
-    };
 
+    this.compressImage(event.target.files[0])
+      .pipe(take(1))
+      .subscribe(headerCompressedImage => {
+        this.HeaderFormat = headerCompressedImage.name.split('.').pop();
+        this.headerFileName = headerCompressedImage.name;
+        this.HeadersizeInMB = this.sharedServices.formatBytes(event.target.files[0].size);
+        
+        const reader = new FileReader();
+        reader.readAsDataURL(event.target.files[0]);
+        reader.onloadend = () => {
+          const data: string = reader.result as string;
+          this.header = data.substring(data.indexOf(',') + 1);
+          const img = new Image();
+          img.src = reader.result as string;
+          img.onload = () => {
+            this.headerHeight = img.naturalHeight;
+            this.headerWidth = img.naturalWidth;
+            if (!this.HeadersizeInMB.includes('KB')
+              // ||
+              //   !(this.headerHeight <= 100 && this.headerWidth <= 500)
+            ) {
+              this.isHeaderNotSubmitted = false;
+              if (this.HeadersizeInMB.includes('KB')) {
+                var hdSize = parseFloat(this.HeadersizeInMB.replace("KB", "").trim());
+                if (hdSize > 300) {
+                  this.isHeaderSize = true;
+                  this.isHeaderFormat = false;
+                }
+              }
+              else {
+                this.isHeaderSize = true;
+                this.isHeaderFormat = false;
+              }
+              // if (!(this.headerHeight <= 100 && this.headerWidth <= 500)) this.isHeaderDimension = true;
+            } else {
+              if (this.HeadersizeInMB.includes('KB')) {
+                var hdSize = parseFloat(this.HeadersizeInMB.replace("KB", "").trim());
+                if (hdSize > 300) {
+                  this.isHeaderSize = true;
+                }
+              } else {
+                this.isHeaderSize = false;
+              }
+
+              // this.headerFile = event.target.files[0];
+              this.headerFile = headerCompressedImage;
+              // this.isHeaderDimension = false;
+              this.isHeaderNotSubmitted = false;
+              this.isHeaderFormat = false;
+            }
+          };
+        };
+
+
+      });
 
   }
 
   selectFooterFile(event) {
-    this.footerFileName = event.target.files[0].name;
-    this.FooterFormat = event.target.files[0].name.split('.')[1];
-    this.FootersizeInMB = this.sharedServices.formatBytes(event.target.files[0].size);
-    if (!this.ImageFormat.includes(this.FooterFormat.toLowerCase())) {
+    if (!this.ImageFormat.includes(event.target.files[0].name.split('.').pop().toLowerCase())) {
       this.isFooterFormat = true;
       this.isFooterSize = false;
-      this.isFooterDimension = false;
+      // this.isFooterDimension = false;
       this.isFooterNotSubmitted = false;
     }
-    const reader = new FileReader();
-    reader.readAsDataURL(event.target.files[0]);
-    reader.onloadend = () => {
-      const data: string = reader.result as string;
-      this.footer = data.substring(data.indexOf(',') + 1);
-      const img = new Image();
-      img.src = reader.result as string;
-      img.onload = () => {
-        this.footerHeight = img.naturalHeight;
-        this.footerWidth = img.naturalWidth;
-        if (!this.FootersizeInMB.includes('KB') ||
-          !(this.footerHeight <= 100 && this.footerWidth <= 500)) {
-          this.isFooterNotSubmitted = false;
+    this.compressImage(event.target.files[0])
+      .pipe(take(1))
+      .subscribe(footerCompressedImage => {
+        this.footerFileName = footerCompressedImage.name;
+        this.FooterFormat = footerCompressedImage.name.split('.')[1];
+        this.FootersizeInMB = this.sharedServices.formatBytes(event.target.files[0].size);
 
-          if (this.FootersizeInMB.includes('KB')) {
-            var ftSize = parseFloat(this.FootersizeInMB.replace("KB", "").trim());
-            if (ftSize > 300) {
-              this.isFooterSize = true;
+        const reader = new FileReader();
+        reader.readAsDataURL(event.target.files[0]);
+        reader.onloadend = () => {
+          const data: string = reader.result as string;
+          this.footer = data.substring(data.indexOf(',') + 1);
+          const img = new Image();
+          img.src = reader.result as string;
+          img.onload = () => {
+            this.footerHeight = img.naturalHeight;
+            this.footerWidth = img.naturalWidth;
+            if (!this.FootersizeInMB.includes('KB')
+              // ||
+              //   !(this.footerHeight <= 100 && this.footerWidth <= 500)
+            ) {
+              this.isFooterNotSubmitted = false;
+
+              if (this.FootersizeInMB.includes('KB')) {
+                var ftSize = parseFloat(this.FootersizeInMB.replace("KB", "").trim());
+                if (ftSize > 300) {
+                  this.isFooterSize = true;
+                  this.isFooterFormat = false;
+                }
+              }
+              else {
+                this.isFooterSize = true;
+                this.isFooterFormat = false;
+              }
+              // if (!(this.footerHeight <= 100 && this.footerWidth <= 500)) this.isFooterDimension = true;
+            } else {
+              if (this.FootersizeInMB.includes('KB')) {
+                var ftSize = parseFloat(this.FootersizeInMB.replace("KB", "").trim());
+                if (ftSize > 300) {
+                  this.isFooterSize = true;
+                }
+              } else {
+                this.isFooterSize = false;
+              }
+
+              this.footerFile = event.target.files[0];
+              // this.isFooterDimension = false;
+              this.isFooterNotSubmitted = false;
+              this.isFooterFormat = false;
             }
-          }
-          if (!(this.footerHeight <= 100 && this.footerWidth <= 500)) this.isFooterDimension = true;
-        } else {
-          this.footerFile = event.target.files[0];
-          this.isFooterSize = false;
-          this.isFooterDimension = false;
-          this.isFooterNotSubmitted = false;
-          this.isFooterFormat = false;
-        }
-      };
-    };
+          };
+        };
+      });
 
   }
 
@@ -250,7 +318,7 @@ export class NphiesAttachmentConfigurationComponent implements OnInit {
     this.headerFileName = "";
     this.HeadersizeInMB = "";
     this.header = "";
-    this.isHeaderDimension = false;
+    // this.isHeaderDimension = false;
     this.isHeaderSize = false;
     this.isHeaderNotSubmitted = false;
     this.isHeaderFormat = false;
@@ -266,7 +334,7 @@ export class NphiesAttachmentConfigurationComponent implements OnInit {
     this.footerFileName = "";
     this.FootersizeInMB = "";
     this.footer = "";
-    this.isFooterDimension = false;
+    // this.isFooterDimension = false;
     this.isFooterSize = false;
     this.isFooterNotSubmitted = false;
     this.isFooterFormat = false;
@@ -293,7 +361,11 @@ export class NphiesAttachmentConfigurationComponent implements OnInit {
       if (!this.headerFile) this.isHeaderNotSubmitted = true;
       if (!this.footerFile) this.isFooterNotSubmitted = true;
     }
-    else if (!this.isHeaderSize && !this.isHeaderDimension && !this.isFooterSize && !this.isFooterDimension && !this.isHeaderFormat && !this.isFooterFormat) {
+    else if (!this.isHeaderSize &&
+      //  !this.isHeaderDimension &&
+      !this.isFooterSize &&
+      //  !this.isFooterDimension &&
+      !this.isHeaderFormat && !this.isFooterFormat) {
       this.onSubmit();
     }
 
@@ -340,6 +412,62 @@ export class NphiesAttachmentConfigurationComponent implements OnInit {
         this.sharedServices.loadingChanged.next(false);
       }
     });
+  }
+
+  compressImage(file: File): Observable<File> {
+    const imageType = file.type || 'image/jpeg'
+    const reader = new FileReader()
+    reader.readAsDataURL(file)
+
+    return Observable.create(observer => {
+      // This event is triggered each time the reading operation is successfully completed.
+      reader.onload = ev => {
+        // Create an html image element
+        const img = this.createImage(ev)
+        // Choose the side (width or height) that longer than the other
+        const imgWH = img.width > img.height ? img.width : img.height
+
+        // Determines the ratios to compress the image
+        let withHeightRatio = (imgWH > widthHeightMax) ? widthHeightMax / imgWH : defaultWidthHeightRatio
+        let qualityRatio = (file.size > fileSizeMax) ? fileSizeMax / file.size : defaultQualityRatio
+
+        // Fires immediately after the browser loads the object
+        img.onload = () => {
+          const elem = document.createElement('canvas')
+          // resize width, height
+          elem.width = img.width * withHeightRatio
+          elem.height = img.height * withHeightRatio
+
+          const ctx = <CanvasRenderingContext2D>elem.getContext('2d')
+          ctx.drawImage(img, 0, 0, elem.width, elem.height)
+          ctx.canvas.toBlob(
+            // callback, called when blob created
+            blob => {
+              observer.next(new File(
+                [blob],
+                file.name,
+                {
+                  type: imageType,
+                  lastModified: Date.now(),
+                }
+              ))
+            },
+            imageType,
+            qualityRatio, // reduce image quantity
+          )
+        }
+      }
+
+      // Catch errors when reading file
+      reader.onerror = error => observer.error(error)
+    })
+  }
+
+  private createImage(ev) {
+    let imageContent = ev.target.result
+    const img = new Image()
+    img.src = imageContent
+    return img
   }
 
 }
