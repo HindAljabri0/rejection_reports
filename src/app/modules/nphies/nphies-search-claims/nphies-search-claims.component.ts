18a825e5bdaf90231f115712f6b165e184814ba9
mohammed.mahjoob <mohammed.mahjoob@tirhal.net>
2022-12-15 15:08:55 +0300

    fix memberid filter

--- src/app/modules/nphies/nphies-search-claims/nphies-search-claims.component.ts
+++ src/app/modules/nphies/nphies-search-claims/nphies-search-claims.component.ts
@@ -358,7 +358,7 @@ export class NphiesSearchClaimsComponent implements OnInit, AfterViewChecked, On
 
     this.claimSearchCriteriaModel.patientFileNo = this.params.patientFileNo;
 
-    this.claimSearchCriteriaModel.memberId = this.params.filter_memberId || this.params.memberId;
+    this.claimSearchCriteriaModel.memberId = this.params.memberId;
 
     this.claimSearchCriteriaModel.documentId = this.params.nationalId;
     this.claimSearchCriteriaModel.requestBundleId = this.params.requestBundleId;
