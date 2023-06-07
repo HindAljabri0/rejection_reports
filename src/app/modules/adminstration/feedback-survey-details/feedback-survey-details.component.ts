1eefa2cf90c19237d8c3b13241d96c255572b7fd
Akshat Mewada <akshat.mewada@tatvasoft.com>
2023-06-07 15:44:14 +0530

    Added feedback survey related screens

---
+++ src/app/modules/adminstration/feedback-survey-details/feedback-survey-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-feedback-survey-details',
+  templateUrl: './feedback-survey-details.component.html',
+  styles: []
+})
+export class FeedbackSurveyDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
