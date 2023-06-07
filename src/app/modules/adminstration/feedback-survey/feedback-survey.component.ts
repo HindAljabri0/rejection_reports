38cd697566022f86f19112cf753381108039d28e
Akshat Mewada <akshat.mewada@tatvasoft.com>
2023-06-07 17:59:35 +0530

    Modified feedback dialog

--- src/app/modules/adminstration/feedback-survey/feedback-survey.component.ts
+++ src/app/modules/adminstration/feedback-survey/feedback-survey.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material';
+import { FeedbackDialogComponent } from 'src/app/components/dialogs/feedback-dialog/feedback-dialog.component';
 
 @Component({
   selector: 'app-feedback-survey',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FeedbackSurveyComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private dialog: MatDialog
+  ) { }
 
   ngOnInit() {
+  }
+
+  openPreviewDialog() {
+    const dialogRef = this.dialog.open(FeedbackDialogComponent, {
+      panelClass: ['primary-dialog', 'dialog-lg']
+    });
   }
 
 }
