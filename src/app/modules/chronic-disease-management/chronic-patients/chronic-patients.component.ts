93319c7cc053d94881b0500b9d855a00991015df
mohammed.mahjoob <mohammed.mahjoob@tirhal.net>
2023-07-05 14:20:30 +0300

    cdm download part

--- src/app/modules/chronic-disease-management/chronic-patients/chronic-patients.component.ts
+++ src/app/modules/chronic-disease-management/chronic-patients/chronic-patients.component.ts
@@ -7,6 +7,8 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 import { HttpErrorResponse, HttpResponse } from '@angular/common/http';
 import { SharedDataService } from 'src/app/services/sharedDataService/shared-data.service';
 import { SettingsService } from 'src/app/services/settingsService/settings.service';
+import { DownloadService } from 'src/app/services/downloadService/download.service';
+import { DownloadStatus } from 'src/app/models/downloadRequest';
 
 @Component({
   selector: 'app-chronic-patients',
@@ -26,6 +28,7 @@ export class ChronicPatientsComponent implements OnInit {
   regionError: string;
   diagnosisError: string;
   thereIsError: boolean = false;
+  detailTopActionIcon: string;
 
   Regions: { Code: string, Name: string }[] = [
     { Code: 'East', Name: 'East' },
@@ -34,9 +37,9 @@ export class ChronicPatientsComponent implements OnInit {
     { Code: 'Center', Name: 'Center' },
     { Code: 'North', Name: 'North' }
   ];
-  Cities: { Code: string,Region:string, Name: string }[] = this.sharedData.Cities;
-  filterdCities: { Code: string,Region:string, Name: string }[] ;
-  selectedRegion='';
+  Cities: { Code: string, Region: string, Name: string }[] = this.sharedData.Cities;
+  filterdCities: { Code: string, Region: string, Name: string }[];
+  selectedRegion = '';
   diagnosisList = [];
   //{ Codes: string[], Name: string }[]
   patientList: any;
@@ -49,17 +52,18 @@ export class ChronicPatientsComponent implements OnInit {
   constructor(
     private dialog: MatDialog,
     private sharedServices: SharedServices,
-    private sharedData:SharedDataService,
+    private sharedData: SharedDataService,
     private cdmService: CdmService,
-    private settingService:SettingsService,
+    private downloadService: DownloadService,
+    private settingService: SettingsService,
     private formBuilder: FormBuilder
   ) { }
 
   ngOnInit() {
     this.fetchDiagnosis();
   }
-  
-  fetchDiagnosis(){
+
+  fetchDiagnosis() {
     this.settingService.getAllDiagnosisList().subscribe(event => {
       if (event instanceof HttpResponse) {
         console.log(event.body);
@@ -93,17 +97,17 @@ export class ChronicPatientsComponent implements OnInit {
     this.diagnosis = currentRow.diagnosis;
     console.log(JSON.stringify(this.diagnosis));
   }
-  filterCities(){
-    console.log("Selected Region = "+this.selectedRegion);
-    this.filterdCities= this.Cities.filter(f => f.Region === this.selectedRegion);
+  filterCities() {
+    console.log("Selected Region = " + this.selectedRegion);
+    this.filterdCities = this.Cities.filter(f => f.Region === this.selectedRegion);
   }
   getData() {
     const model: any = {};
-    this.thereIsError=false;
-    this.policyError=null;
-    this.cityError=null;
-    this.regionError=null;
-    this.diagnosisError =null;
+    this.thereIsError = false;
+    this.policyError = null;
+    this.cityError = null;
+    this.regionError = null;
+    this.diagnosisError = null;
 
     if (this.cdmForm.controls.policyNumber.value) {
       model.policyNumber = this.cdmForm.controls.policyNumber.value;
@@ -113,8 +117,8 @@ export class ChronicPatientsComponent implements OnInit {
     }
     if (this.cdmForm.controls.city.value) {
       model.city = this.cdmForm.controls.city.value;
-    } else{
-      model.city =null;
+    } else {
+      model.city = null;
     }
     if (this.cdmForm.controls.region.value) {
       model.region = this.cdmForm.controls.region.value;
@@ -156,8 +160,58 @@ export class ChronicPatientsComponent implements OnInit {
     const dialogRef = this.dialog.open(ChronicPatientDetailsComponent, {
       panelClass: ['primary-dialog', 'full-screen-dialog'],
       autoFocus: false,
-      data:{patientId:_patientId}
+      data: { patientId: _patientId }
     });
   }
+  async downloadSheetFormat() {
+    const model: any = {};
+    this.thereIsError = false;
+    this.policyError = null;
+    this.cityError = null;
+    this.regionError = null;
+    this.diagnosisError = null;
+    let event;
 
+    if (this.cdmForm.controls.policyNumber.value) {
+      model.policyNumber = this.cdmForm.controls.policyNumber.value;
+    } else {
+      this.policyError = 'Please fill policy number';
+      this.thereIsError = true;
+    }
+    if (this.cdmForm.controls.city.value) {
+      model.city = this.cdmForm.controls.city.value;
+    } else {
+      model.city = null;
+    }
+    if (this.cdmForm.controls.region.value) {
+      model.region = this.cdmForm.controls.region.value;
+    } else {
+      this.regionError = 'Please select region';
+      this.thereIsError = true;
+    }
+    if (this.cdmForm.controls.diagnosis.value) {
+      model.diagnosis = this.cdmForm.controls.diagnosis.value;
+      model.diagnosis = JSON.parse(model.diagnosis);
+      //odel.diagnosis = model.diagnosis.split(',');
+    } else {
+      this.diagnosisError = 'Please select diagnosis';
+      this.thereIsError = true;
+    }
+    if (this.cdmForm.controls.memberId.value) {
+      model.memberId = this.cdmForm.controls.memberId.value;
+    }
+    if (!this.thereIsError) {
+      event = this.cdmService.downloadExcelsheet(this.sharedServices.providerId, model);
+      if (event != null) {
+        this.downloadService.startGeneratingDownloadFile(event)
+          .subscribe(status => {
+            if (status !== DownloadStatus.ERROR) {
+              this.detailTopActionIcon = 'ic-check-circle.svg';
+            } else {
+              this.detailTopActionIcon = 'ic-download.svg';
+            }
+          });
+      }
+    }
+  }
 }
--- src/app/services/cdmService/cdm.service.ts
+++ src/app/services/cdmService/cdm.service.ts
@@ -9,14 +9,30 @@ export class CdmService {
 
   constructor(private http: HttpClient) { }
 
-  getPatientList(providerId: string, body: any,page,size){
+  getPatientList(providerId: string, body: any, page, size) {
     const requestUrl = `/cdm/providers/${providerId}/search?page=${page}&size=${size}`;
     const request = new HttpRequest('POST', environment.chronicDiseaseManagement + requestUrl, body);
     return this.http.request(request);
   }
-  getPatientApproval(providerId: string, _patientId: string){
+  getPatientApproval(providerId: string, _patientId: string) {
     const requestUrl = `/cdm/providers/${providerId}/detail`;
-    const request = new HttpRequest('POST', environment.chronicDiseaseManagement + requestUrl, {patientId : _patientId});
+    const request = new HttpRequest('POST', environment.chronicDiseaseManagement + requestUrl, { patientId: _patientId });
     return this.http.request(request);
   }
+  downloadExcelsheet(providerId: string, model: any) {
+    let requestURL = `/cdm/providers/${providerId}/download?`;
+
+    if (model.city != null) {
+      requestURL += `&city=${model.city}`;
+    }
+    if (model.policyNumber != null) {
+      requestURL += `&policyNumber=${model.policyNumber}`;
+    }
+    if (model.diagnosis != null) {
+      requestURL += `&diagnosis=${model.diagnosis}`;
+    }
+    const request = new HttpRequest('GET', environment.chronicDiseaseManagement + requestURL, '', { responseType: 'text', reportProgress: true });
+    return this.http.request(request);
+  }
+
 }
