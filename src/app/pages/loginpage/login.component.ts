16443f3c3507e4c1929df3e877c33ac4406c350c
omarWaseel <omar.alohideb@waseel.com>
2023-09-11 09:35:03 +0300

    handel excption 401 406 when login

--- src/app/pages/loginpage/login.component.ts
+++ src/app/pages/loginpage/login.component.ts
@@ -114,7 +114,7 @@ export class LoginComponent implements OnInit {
           this.errors = 'Your account has been blocked, kindly contact Waseel Customer Care!';
         }
         else if  (errorEvent.status == 401 || errorEvent.status == 406) {
-          this.errors = 'Please use http://sso.waseel.com to resolve your issue!';
+          this.errors = 'Please use  http://sso.waseel.com/ to resolve your issue!';
         }
         else if (errorEvent.status < 500 && errorEvent.status >= 406) {
           this.errors = 'Token is invalid!';
@@ -162,7 +162,7 @@ export class LoginComponent implements OnInit {
           this.errors = 'Your account has been blocked, kindly contact Waseel Customer Care!';
         }
         else if  (errorEvent.status == 401 || errorEvent.status == 406) {
-          this.errors = 'Please use http://sso.waseel.com to resolve your issue!';
+          this.errors = 'Please use http://sso.waseel.com/ to resolve your issue!';
         }
         else if (errorEvent.status < 500 && errorEvent.status >= 406) {
           this.errors = 'Username or Password is invalid!';
