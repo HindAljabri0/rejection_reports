8cae61f3fd37b05a55b30cb6ab6d37a22938c6b8
godbinahila <godbinah@waseel.com>
2023-09-25 15:28:31 +0530

    fixed config issue

--- src/app/modules/chronic-disease-management/cdm-provider-config/cdm-provider-config.component.ts
+++ src/app/modules/chronic-disease-management/cdm-provider-config/cdm-provider-config.component.ts
@@ -5,6 +5,7 @@ import { SuperAdminService } from 'src/app/services/administration/superAdminSer
 import { CdmService } from 'src/app/services/cdmService/cdm.service';
 import { HttpErrorResponse, HttpResponse } from '@angular/common/http';
 import { DialogService } from 'src/app/services/dialogsService/dialog.service';
+import { finalize } from 'rxjs/operators';
 
 
 @Component({
@@ -46,6 +47,8 @@ export class CdmProviderConfigComponent implements OnInit {
   constructor(private sharedServices: SharedServices, private superAdmin: SuperAdminService, private cdmService: CdmService, private dialogService: DialogService, private elementRef: ElementRef) { }
 
   ngOnInit() {
+    this.fetchDiagnosis();
+    this.fetchRegions();
     this.superAdmin.getProviders().subscribe(event => {
       if (event instanceof HttpResponse) {
         if (event.body instanceof Array) {
@@ -61,23 +64,34 @@ export class CdmProviderConfigComponent implements OnInit {
     });
   }
 
-  selectProvider(providerId: string = null) {
+
+  async delay(ms: number) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+  
+  async fetchDataAndHideLoader() {
+    await this.getAllList();
+    await this.delay(1000); // Delay for 500 milliseconds
+    this.sharedServices.loadingChanged.next(false);
+  }
+  
+  async selectProvider(providerId: string = null) {
+    this.sharedServices.loadingChanged.next(true);
     if (providerId !== null) {
       this.selectedProvider = providerId;
     } else {
       const providerId = this.providerController.value.split('|')[0].trim();
       this.selectedProvider = providerId;
-    }
-    this.fetchDiagnosis();
-    this.fetchRegions();
-    this.diagSelectedItems = [];
-      this.regSelectedItems  = [];
-      this.policySelectedItems = [];
-     setTimeout(() => {
-      this.getAllList();
-    }, 500);
+    }  
 
+    this.diagSelectedItems = [];
+    this.regSelectedItems = [];
+    this.policySelectedItems = [];
+    await this.fetchDataAndHideLoader();
   }
+  
+
+  
 
   updateFilter() {
     this.filteredProviders = this.providers.filter(provider =>
@@ -101,7 +115,7 @@ export class CdmProviderConfigComponent implements OnInit {
   }
 
   getAllList() {
-       this.sharedServices.loadingChanged.next(true);
+    this.sharedServices.loadingChanged.next(true);
     this.superAdmin.getAllList(this.selectedProvider).subscribe(event => {
       if (event instanceof HttpResponse) {
         if (event.body instanceof Array) {
@@ -151,24 +165,74 @@ export class CdmProviderConfigComponent implements OnInit {
           this.regions = regionsFilter;
           this.initialDiagSelectedItems = [...this.diagSelectedItems];
           this.initialRegSelectedItems = [...this.regSelectedItems];
-          this.initialPolicySelectedItems = [...this.policySelectedItems];
-          this.sharedServices.loadingChanged.next(false);
+          this.initialPolicySelectedItems = [...this.policySelectedItems];     
         }
       }
     },
       (error) => {
         console.error('getAllList error:', error);
+      },
+
+      () => {
+        this.regionFlag = false;
+        this.diagnosisFlag = false;
+        this.fetchFlag = false;
+        this.sharedServices.loadingChanged.next(false);
+      });
+    
+  }
+
+  onRegionChange(item: any) {
+    item.regSelected = !item.regSelected;
+    if (item.regSelected) {
+      this.regSelectedItems.unshift(item);
+      this.regions = this.regions.filter(region => region !== item);
+      this.selectedValues.push(item.regionCode); 
+    } else {
+      const index = this.regSelectedItems.indexOf(item);
+      if (index !== -1) {
+        this.regSelectedItems.splice(index, 1);
       }
-    );
+      const regionCode = item.regionCode; 
+      const selectedIndex = this.selectedValues.indexOf(regionCode);
+      if (selectedIndex !== -1) {
+        this.selectedValues.splice(selectedIndex, 1); 
+      }
+    }
+  }
+
+  onDiagChange(item: any) {
+    item.diagSelected = !item.diagSelected;
+    if (item.diagSelected) {
+      this.diagSelectedItems.unshift(item);
+      this.diagnosisList = this.diagnosisList.filter(diagnosis => diagnosis !== item);
+      this.selectedValues.push(item.diagnosisCode); 
+    } else {
+      const index = this.diagSelectedItems.indexOf(item);
+      if (index !== -1) {
+        this.diagSelectedItems.splice(index, 1);
+      }
+      const diagnosisCode = item.diagnosisCode; 
+      const selectedIndex = this.selectedValues.indexOf(diagnosisCode);
+      if (selectedIndex !== -1) {
+        this.selectedValues.splice(selectedIndex, 1); 
+      }
+    }
+  }
+
+  fetchPolicy() {
+    this.fetchFlag = true;
+    this.regionFlag = false;
+    this.diagnosisFlag = false;
   }
   fetchDiagnosis() {
     this.sharedServices.loadingChanged.next(true);
-    this.superAdmin.getAllDiagnosis(this.selectedProvider).subscribe(event => {
+      this.superAdmin.getAllDiagnosis(this.selectedProvider).subscribe(event => {
       if (event instanceof HttpResponse) {
-        if (event.body != null) {
-          this.diagnosisFlag = true;
+        this.diagnosisFlag = true;
           this.regionFlag = false;
           this.fetchFlag = false;
+        if (event.body != null) {
           this.diagnosisList = event.body;
           const newDiagnosisList = this.diagnosisList.filter(item1 => !this.diagSelectedItems.some(item2 => item1.diagnosisDescription === item2.diagnosisDescription));
           this.diagnosisList = newDiagnosisList;
@@ -185,60 +249,25 @@ export class CdmProviderConfigComponent implements OnInit {
         }
       });
   }
-
-  onRegionChange(item: any) {
-    item.regSelected = !item.regSelected;
-    if (item.regSelected) {
-      this.regSelectedItems.unshift(item);
-      this.regions = this.regions.filter(region => region !== item);
-    } else {
-      const index = this.regSelectedItems.indexOf(item);
-      if (index !== -1) {
-        this.regSelectedItems.splice(index, 1);
-      }
-    }
-  }
-
-  onDiagChange(item: any) {
-    item.diagSelected = !item.diagSelected;
-    if (item.diagSelected) {
-      this.diagSelectedItems.unshift(item);
-      this.diagnosisList = this.diagnosisList.filter(diagnosis => diagnosis !== item);
-    } else {
-      const index = this.diagSelectedItems.indexOf(item);
-      if (index !== -1) {
-        this.diagSelectedItems.splice(index, 1);
-      }
-    }
-  }
-
-  fetchPolicy() {
-    this.fetchFlag = true;
-    this.regionFlag = false;
-    this.diagnosisFlag = false;
-  }
-
   fetchRegions() {
+    this.sharedServices.loadingChanged.next(true);
     this.superAdmin.getAllRegion(this.selectedProvider).subscribe(event => {
       if (event instanceof HttpResponse) {
         this.regionFlag = true;
         this.diagnosisFlag = false;
         this.fetchFlag = false;
-
         if (event.body != null)
           this.regions = event.body;
         const newRegionList = this.regions.filter(item1 => !this.regSelectedItems.some(item2 => item1.regionDescription === item2.regionDescription));
         this.regions = newRegionList;
-
         for (let item of this.regions) {
           item.regSelected = false;
         }
+        this.sharedServices.loadingChanged.next(false);
       }
     }
       , err => {
-
         if (err instanceof HttpErrorResponse) {
-
           this.regions = null;
         }
       });
@@ -277,12 +306,52 @@ export class CdmProviderConfigComponent implements OnInit {
     this.policySelectedItems.splice(index, 1);
   }
 
-  reset() {
+  reset(diag: boolean, policy: boolean, region: boolean) {
+       if(diag){
+        this.fetchDiagnosis();
+      this.regionFlag = false;
+      this.diagnosisFlag = true;
+      this.fetchFlag = false;
+    }
+    if(policy){
+      this.regionFlag = false;
+      this.diagnosisFlag = false;
+      this.fetchFlag = true;
+
+    }
+    if(region){
+      this.fetchRegions();
+      this.regionFlag = true;
+      this.diagnosisFlag = false;
+      this.fetchFlag = false;
+      
+    }
+    this.diagSelectedItems.forEach(item => {
+      const index = this.selectedValues.indexOf(item.diagnosisCode);
+      if (index !== -1) {
+        this.selectedValues.splice(index, 1);
+      }
+    });
+  
+    this.regSelectedItems.forEach(item => {
+      const index = this.selectedValues.indexOf(item.regionCode);
+      if (index !== -1) {
+        this.selectedValues.splice(index, 1);
+      }
+    });
+  
+    this.policySelectedItems.forEach(item => {
+      const index = this.selectedValues.indexOf(item.policyCode);
+      if (index !== -1) {
+        this.selectedValues.splice(index, 1);
+      }
+    });
+  
     this.diagSelectedItems = [...this.initialDiagSelectedItems];
     this.regSelectedItems = [...this.initialRegSelectedItems];
     this.policySelectedItems = [...this.initialPolicySelectedItems];
-   }
-
+    }
+  
   save() {
     const diagArray = this.diagSelectedItems.map(item => ({
       'diagnosisDescription': item.diagnosisDescription,
@@ -307,13 +376,20 @@ export class CdmProviderConfigComponent implements OnInit {
     };
     this.savePopupOpen = true;
     this.sharedServices.loadingChanged.next(true);
-    this.superAdmin.saveCdmCategories(this.selectedProvider, dataToSave).subscribe(event => {
-      if (event instanceof HttpResponse) {
-        if (event.status == 201 || event.status) {
-          this.dialogService.openMessageDialog({
-            title: '',
-            message: `Data saved successfully`,
-            isError: false,
+    this.superAdmin.saveCdmCategories(this.selectedProvider, dataToSave)
+      .pipe(
+        finalize(() => {
+          this.sharedServices.loadingChanged.next(false);
+          this.getAllList(); 
+        })
+      )
+      .subscribe(event => {
+        if (event instanceof HttpResponse) {
+          if (event.status == 201 || event.status) {
+            this.dialogService.openMessageDialog({
+              title: '',
+              message: `Data saved successfully`,
+              isError: false,
           });
         }
         this.sharedServices.loadingChanged.next(false);
@@ -322,8 +398,13 @@ export class CdmProviderConfigComponent implements OnInit {
       (error) => {
         console.error('Error saving data:', error);
       });
+    this.selectedValues = this.selectedValues.filter(value => {
+      const existsInDiag = diagArray.some(item => item.diagnosisCode === value);
+      const existsInReg = regArray.some(item => item.regionCode === value);
+      const existsInPolicy = policyArray.some(item => item.policyCode === value);
+      return !(existsInDiag || existsInReg || existsInPolicy);
+    });
   }
-
 
   filterCodes() {
     if (this.filterWLECodesControl.value != null) {
